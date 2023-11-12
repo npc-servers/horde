5b289e890b75b86dac63dafb71432d64fd6783bc
Jagnt <148128844+Jagnt@users.noreply.github.com>
2023-11-12 17:54:28 -0500

    Moving this to other places

--- gamemodes/horde/entities/entities/horde_skull_token/shared.lua
+++ gamemodes/horde/entities/entities/horde_skull_token/shared.lua
@@ -1,12 +1,2 @@
 ENT.Type = "anim"
 ENT.Base = "base_anim"
-
-if SERVER then
-    util.AddNetworkString("PrintSkullNotify")
-end
-
-if CLIENT then
-    net.Receive("PrintSkullNotify", function ()
-        chat.AddText(Color(255,0,255), "You found a skull token!")
-    end)
-end
