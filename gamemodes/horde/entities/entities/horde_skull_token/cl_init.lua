5adf7b73a86f6835fe3e405d1bc4a049f3d8340f
Jagnt <148128844+Jagnt@users.noreply.github.com>
2023-11-12 17:56:33 -0500

    Moved net.Receive for skull notify to cl_init

--- gamemodes/horde/entities/entities/horde_skull_token/cl_init.lua
+++ gamemodes/horde/entities/entities/horde_skull_token/cl_init.lua
@@ -5,3 +5,9 @@ function ENT:Draw()
 end
 
 ENT.ColorModulation = Color(1, 0, 1)
+
+if CLIENT then
+    net.Receive("PrintSkullNotify", function ()
+        chat.AddText(Color(255,0,255), "You found a skull token!")
+    end)
+end
