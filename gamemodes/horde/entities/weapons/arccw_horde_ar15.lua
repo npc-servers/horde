38f1f0516d4333658456898dbabb52fbc7c4dbf5
The1337Gh0st <cwarrior1414@gmail.com>
2023-12-02 03:56:18 -0500

    Ghost / misc weapon changes / fixes
    Medkit now heals for 20% of max health rather than a flat 20, increased range by 50%
    
    Edited Flare Gun's description to mention the new reserve ammo mechanic, fixed projectile collision group
    
    Fixed Apollo's projectile collision group, buffed Apollo damage to 60, reduced recoil, adjusted accuracy a bit to compensate
    
    Fixed OSIPR's projectile collision group, reduced recoil, buffed moving / hip accuracy
    
    Fixed M16A4 Grenadier not being able to use bullet types
    
    Buffed M1911 damage to 65-50, increased chamber size to 1, fixed not being able to use bullet types / perks
    
    Buffed AR15 firerate to 0.1
    
    Buffed Pump-Action damage to 10x7 (70) - 6x7 (42), reduced weight to 2, reduced ammo cost to 2
    
    Buffed 357 damage to 60-50, reduced ammo cost to 2
    
    Reduced recoil on AWSM, removed code that forced you to equip the scope on equip
    
    Ghost's semi-auto weapons now have access to full auto and burst weapon perks
    
    Scar-H firerate ~0.092 > 0.11, added full-auto firing mode, reduced damage to 55-40 to compensate, increased price to 2750
    
    G3 firerate 0.109 > 0.23
    
    FN FAL firerate 0.1 > 0.15, added full auto, reduced damage to 65-55 to compensate
    
    Reduced M79 price to 1500, reduced weight to 5
    
    Buffed Winchester LAR damage to 150-125
    
    Buffed Striker's reload speed
    
    Changed Antlion's projectile collision type to COLLISION_GROUP_PLAYER_MOVEMENT (does not collide with players)
    
    Reduced the recoil of the SSG08 (medic and normal)
    
    Fixed Berserker Armor's screen color effect lasting for 8 seconds instead of 10 seconds

--- gamemodes/horde/entities/weapons/arccw_horde_ar15.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_ar15.lua
@@ -46,7 +46,7 @@ SWEP.RecoilSide = 0.260
 SWEP.RecoilRise = 0.1
 SWEP.RecoilPunch = 2.5
 
-SWEP.Delay = 0.15 -- 60 / RPM.
+SWEP.Delay = 0.1 -- 60 / RPM.
 SWEP.Num = 1 -- number of shots per trigger pull.
 SWEP.Firemodes = {
     {
