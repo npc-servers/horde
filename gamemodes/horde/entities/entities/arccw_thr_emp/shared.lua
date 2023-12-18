afd3113df497affb919ce978aa7f51aa50a4d15b
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2023-12-18 04:32:21 -0500

    enough to make it do over 100

--- gamemodes/horde/entities/entities/arccw_thr_emp/shared.lua
+++ gamemodes/horde/entities/entities/arccw_thr_emp/shared.lua
@@ -67,7 +67,7 @@ function ENT:Explode()
 
     for _, e in pairs(ents.FindInSphere(self:GetPos(), 175)) do
         if IsValid(e) and HORDE:IsEnemy(e) then
-            for i = 1, 12 do
+            for i = 1, 13 do
                 local dmginfo = DamageInfo()
                 dmginfo:SetDamage(8)
                 dmginfo:SetDamageType(DMG_SHOCK)
