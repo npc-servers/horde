a9d0270132354bbb2529ef044cbbf2df79456a3b
Redox <mail@redox.to>
2023-12-24 19:12:17 +0100

    Replace difficulty_text logic

--- gamemodes/horde/gamemode/sh_difficulty.lua
+++ gamemodes/horde/gamemode/sh_difficulty.lua
@@ -1,3 +1,20 @@
--- HORDE.Difficulty = {
-
--- }
+HORDE.Difficulty = {
+    [1] = {
+        name = "NORMAL"
+    },
+    [2] = {
+        name = "HARD"
+    },
+    [3] = {
+        name = "REALISM"
+    },
+    [4] = {
+        name = "NIGHTMARE"
+    },
+    [5] = {
+        name = "APOCALYPSE"
+    },
+    [6] = {
+        name = "HELL"
+    }
+}
