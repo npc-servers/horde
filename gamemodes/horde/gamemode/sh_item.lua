6cb767159cb172b428fbc3b9595f3e7730b2103b
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2024-01-23 02:29:11 -0500

    poll was tied, coinflip resulted in spore launcher cost reduction

--- gamemodes/horde/gamemode/sh_item.lua
+++ gamemodes/horde/gamemode/sh_item.lua
@@ -622,7 +622,7 @@ function HORDE:GetDefaultItemsData()
 
     HORDE:CreateItem("Special",    "Hivehand",       "horde_hivehand",       2000,  5, "Organic weapon used by Xen soldiers.\nHas infinite ammo.\nPrimary fire generates homing ricocheting shots.\nSecondary fire rapidly unloads the entire weapon.",
     {Engineer=true}, 2, -1, nil, nil, {Engineer=4}, nil, {HORDE.DMG_BALLISTIC})
-    HORDE:CreateItem("Special",    "Spore Launcher", "horde_spore_launcher",2500,  6, "Improvised biological weapon.\nShoots out acidic projectiles that explodes after a short delay.\nHeals players and damages enemies.",
+    HORDE:CreateItem("Special",    "Spore Launcher", "horde_spore_launcher", 1750,  6, "Improvised biological weapon.\nShoots out acidic projectiles that explodes after a short delay.\nHeals players and damages enemies.",
     {Medic=true, Survivor=true}, 15, -1, nil, nil, {Medic=2, Survivor=2}, nil, {HORDE.DMG_POISON})
     HORDE:CreateItem("Special",    "M2 Health Thrower", "horde_healingthrower", 3000,  7, "M2-2 Health Thrower.\nAn American man-portable backpack flamethrower converted to heal stuff to death.\nsmells faintly of lime and mint (who hurt you)",
     {Medic=true}, 30, -1, nil, nil, nil, nil, {HORDE.DMG_POISON})
