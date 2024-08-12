4a31e8eeb8d4b8714fcf0af59f30a2e083ddc7c0
Zisomerism <h3lplessalpaca@gmail.com>
2024-08-11 21:02:13 -0500

    Update horde_sticky_launcher.lua
    Fixed sticky launcher spam

--- gamemodes/horde/entities/weapons/horde_sticky_launcher.lua
+++ gamemodes/horde/entities/weapons/horde_sticky_launcher.lua
@@ -157,7 +157,9 @@ function SWEP:PrimaryAttack()
                 end
                 table.insert(self.Stickies, entity)
                 if table.Count(self.Stickies) > 8 then
-                    self.Stickies[1]:Remove()
+					if IsValid(self.Stickies[1]) then -- without this we try to remove a null entity under a few circumstances and break everything :^)
+						self.Stickies[1]:Remove()
+					end
                     table.remove(self.Stickies, 1)
                 end
             end
