27575e25c061ccbf87b090f01f77fbb04b734d97
SleepySophie <77075160+SleepySophie@users.noreply.github.com>
2025-04-18 22:55:54 +0200

    hatcher perk change

--- gamemodes/horde/gamemode/perks/hatcher/hatcher_natural_selection.lua
+++ gamemodes/horde/gamemode/perks/hatcher/hatcher_natural_selection.lua
@@ -11,7 +11,7 @@ PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
         if not HORDE.player_drop_entities[ply:SteamID()] then return end
         for id, ent in pairs(HORDE.player_drop_entities[ply:SteamID()]) do
             if IsValid( ent ) and ent:IsNPC() and (not ent.Horde_Has_NaturalSelection) then
-                ent:SetMaxHealth(ent:GetMaxHealth() * 1.15)
+                ent:SetMaxHealth(ent:GetMaxHealth() * 1.2)
                 ent:SetHealth(ent:GetMaxHealth())
                 ent.Horde_Has_NaturalSelection = true
             end
@@ -24,7 +24,7 @@ PERK.Hooks.Horde_OnUnSetPerk = function(ply, perk)
         if not HORDE.player_drop_entities[ply:SteamID()] then return end
         for id, ent in pairs(HORDE.player_drop_entities[ply:SteamID()]) do
             if ent:IsNPC() and ent.Horde_Has_NaturalSelection then
-                ent:SetMaxHealth(ent:GetMaxHealth() / 1.15)
+                ent:SetMaxHealth(ent:GetMaxHealth() / 1.2)
                 ent:SetHealth(ent:GetMaxHealth())
                 ent.Horde_Has_NaturalSelection = nil
             end
@@ -39,7 +39,7 @@ PERK.Hooks.OnEntityCreated = function (ent)
         if not ent:IsValid() then return end
         local ply = ent:GetNWEntity("HordeOwner")
         if ply:IsPlayer() and ply:Horde_GetPerk("hatcher_natural_selection") and ent:IsNPC() then
-            ent:SetMaxHealth(ent:GetMaxHealth() * 1.15)
+            ent:SetMaxHealth(ent:GetMaxHealth() * 1.2)
             ent:SetHealth(ent:GetMaxHealth())
             ent.Horde_Has_NaturalSelection = true
         end
