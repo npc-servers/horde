258f6476aed00862d7734ccbed42f5471d99e188
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2025-05-17 06:56:42 -0400

    remove smoke from healthrower particles
    removed smoke from healthrower particles to reduce unneeded visual clutter

--- gamemodes/horde/entities/effects/medic_flame.lua
+++ gamemodes/horde/entities/effects/medic_flame.lua
@@ -25,20 +25,6 @@ function EFFECT:Init(data)
 			end)
 		end
 
-		for i = 1, 2 do
-			local smoke = self.Emitter:Add("particles/smokey", Startpos)
-			smoke:SetDieTime(1.5)
-			smoke:SetStartAlpha(50)
-			smoke:SetEndAlpha(0)
-			smoke:SetStartSize(math.Rand(2, 4))
-			smoke:SetEndSize(math.random(70, 90))
-			smoke:SetRoll(math.random(-10, 10))
-			smoke:SetRollDelta(math.random(-10, 10))
-			smoke:SetVelocity(((Hitpos - Startpos):GetNormal() * math.random(500, 800)) + VectorRand() * math.random(1, 60) + Vector(0,0,20))
-			smoke:SetCollide(true)
-			smoke:SetColor(40, 40, 40)
-		end
-
 		self.Emitter:Finish()
 	end
 end
