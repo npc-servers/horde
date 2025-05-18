a35ab81a50c86c30dfba326f3e1ab075489ca8b7
The American Candy Van <125820322+TheAmericanCandyVan@users.noreply.github.com>
2025-05-18 11:51:45 -0400

    healthrower particle tweak again (#248)
    im an idiot and accidentally made the damn fire blue, but players liked the blue fire so i threw it in to the design whilst lowering particle count and making it much more see-through

--- gamemodes/horde/entities/effects/medic_flame.lua
+++ gamemodes/horde/entities/effects/medic_flame.lua
@@ -8,11 +8,11 @@ function EFFECT:Init(data)
 		self.Emitter = ParticleEmitter(Startpos)
 
 
-		for i = 1, 20 do
+		for i = 1, 4 do
 			local gflame = self.Emitter:Add("particles/flamelet1", Startpos)
-			gflame:SetColor(0,100,255)
+			gflame:SetColor(105, 255, 50)
 			gflame:SetDieTime(1)
-			gflame:SetStartAlpha(100)
+			gflame:SetStartAlpha(30)
 			gflame:SetEndAlpha(0)
 			gflame:SetStartSize(math.Rand(0.8, 1.5))
 			gflame:SetEndSize(math.random(50, 75))
@@ -22,6 +22,22 @@ function EFFECT:Init(data)
 			gflame:SetCollide(true)
 			gflame:SetCollideCallback(function()
 			gflame:SetDieTime(0)
+			end)
+		end
+		for i = 1, 10 do
+			local bflame = self.Emitter:Add("particles/flamelet1", Startpos)
+			bflame:SetColor(0,100,255)
+			bflame:SetDieTime(1)
+			bflame:SetStartAlpha(50)
+			bflame:SetEndAlpha(0)
+			bflame:SetStartSize(math.Rand(0.4, 0.75))
+			bflame:SetEndSize(math.random(25, 37.5))
+			bflame:SetRoll(math.random(-10, 10))
+			bflame:SetRollDelta(math.random(-10, 10))
+			bflame:SetVelocity(((Hitpos - Startpos):GetNormal() * math.random(500, 800)) + VectorRand() * math.random(1, 20))
+			bflame:SetCollide(true)
+			bflame:SetCollideCallback(function()
+			bflame:SetDieTime(0)
 			end)
 		end
 
