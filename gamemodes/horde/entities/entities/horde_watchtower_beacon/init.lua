42b235e0c8b3bc26c588f6f7523d7fdffbb11e0a
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-06-10 21:11:14 -0700

    Added tower freezing and optimized code slightly
    After 5 seconds the warden towers will be frozen
    Removed double if statements
    Added CurTime var. so it doesn't need to be called repeatedly
    Slowed down think of some thinks

--- gamemodes/horde/entities/entities/horde_watchtower_beacon/init.lua
+++ gamemodes/horde/entities/entities/horde_watchtower_beacon/init.lua
@@ -27,6 +27,9 @@ function ENT:Initialize()
     self.Horde_WatchTower = true
     self.Horde_NextShockAttack = CurTime()
     self.Horde_ShockAttackInterval = 1
+    self.Horde_LastMovePos = self:GetPos()
+    self.Horde_IdleStart = CurTime()
+    self.Horde_Idle = false
     self:SetColor(Color(0, 255, 0))
 
     if self.Horde_Owner:Horde_GetPerk("warden_restock") then
@@ -42,29 +45,43 @@ function ENT:Initialize()
 end
 
 function ENT:Think()
-    if CurTime() >= self.Horde_NextThink + self.Horde_ThinkInterval then
-        if SERVER then
-            if self.Horde_Owner:IsPlayer() then
-                hook.Run("Horde_WardenWatchtower", self.Horde_Owner, self)
+    local curTime = CurTime()
+
+    if not self.Horde_Idle then
+        local curPos = self:GetPos()
+        if curPos:DistToSqr(self.Horde_LastMovePos) > 1 then
+            self.Horde_IdleStart = curTime
+            self.Horde_LastMovePos = curPos
+        elseif curTime - self.Horde_IdleStart >= 5 then
+            local phys = self:GetPhysicsObject()
+            if IsValid(phys) then
+                phys:EnableMotion(false)
+                self.Horde_Idle = true
             end
         end
-        self.Horde_NextThink = CurTime()
     end
 
-    if self.Horde_EnableShockwave then
-        if CurTime() >= self.Horde_NextShockWave + self.Horde_ShockwaveInterval then
-            local dmg = DamageInfo()
-            dmg:SetAttacker(self.Horde_Owner)
-            dmg:SetInflictor(self)
-            dmg:SetDamageType(DMG_SHOCK)
-            dmg:SetDamage(50)
-            local e = EffectData()
-            e:SetOrigin(self:GetPos())
-            util.Effect("explosion_shock", e, true, true)
-            util.BlastDamageInfo(dmg, self:GetPos(), 160)
-            self.Horde_NextShockWave = CurTime()
+    if curTime >= self.Horde_NextThink + self.Horde_ThinkInterval then
+        if SERVER and self.Horde_Owner:IsPlayer() then
+            hook.Run("Horde_WardenWatchtower", self.Horde_Owner, self)
         end
+        self.Horde_NextThink = curTime
     end
+
+    if self.Horde_EnableShockwave and curTime >= self.Horde_NextShockWave + self.Horde_ShockwaveInterval then
+        local dmg = DamageInfo()
+        dmg:SetAttacker(self.Horde_Owner)
+        dmg:SetInflictor(self)
+        dmg:SetDamageType(DMG_SHOCK)
+        dmg:SetDamage(50)
+        local e = EffectData()
+        e:SetOrigin(self:GetPos())
+        util.Effect("explosion_shock", e, true, true)
+        util.BlastDamageInfo(dmg, self:GetPos(), 160)
+        self.Horde_NextShockWave = curTime
+    end
+
+    self:NextThink(curTime + 0.1)
 end
 
 function ENT:StartTouch(ent)
--- gamemodes/horde/entities/entities/horde_watchtower_mk3/init.lua
+++ gamemodes/horde/entities/entities/horde_watchtower_mk3/init.lua
@@ -25,6 +25,9 @@ function ENT:Initialize()
     self.Horde_WatchTower = true
     self.Horde_NextShockAttack = CurTime()
     self.Horde_ShockAttackInterval = 1
+    self.Horde_LastMovePos = self:GetPos()
+    self.Horde_IdleStart = CurTime()
+    self.Horde_Idle = false
     self:SetColor(Color(0, 150, 255))
 
     if self.Horde_Owner:Horde_GetPerk("warden_restock") then
@@ -39,31 +42,43 @@ function ENT:Initialize()
 end
 
 function ENT:Think()
-    if CurTime() >= self.Horde_NextThink + self.Horde_ThinkInterval then
-        if SERVER then
-            if self.Horde_Owner:IsPlayer() then
-                hook.Run("Horde_WardenWatchtower", self.Horde_Owner, self)
+    local curTime = CurTime()
+
+    if not self.Horde_Idle then
+        local curPos = self:GetPos()
+        if curPos:DistToSqr(self.Horde_LastMovePos) > 1 then
+            self.Horde_IdleStart = curTime
+            self.Horde_LastMovePos = curPos
+        elseif curTime - self.Horde_IdleStart >= 5 then
+            local phys = self:GetPhysicsObject()
+            if IsValid(phys) then
+                phys:EnableMotion(false)
+                self.Horde_Idle = true
             end
         end
-        self.Horde_NextThink = CurTime()
     end
 
-    if self.Horde_EnableShockwave then
-        if CurTime() >= self.Horde_NextShockWave + self.Horde_ShockwaveInterval then
-            local dmg = DamageInfo()
-            dmg:SetAttacker(self.Horde_Owner)
-            dmg:SetInflictor(self)
-            dmg:SetDamageType(DMG_SHOCK)
-            dmg:SetDamage(50)
-            local e = EffectData()
-            e:SetOrigin(self:GetPos())
-            util.Effect("explosion_shock", e, true, true)
-            util.BlastDamageInfo(dmg, self:GetPos(), 160)
-            self.Horde_NextShockWave = CurTime()
+    if curTime >= self.Horde_NextThink + self.Horde_ThinkInterval then
+        if SERVER and self.Horde_Owner:IsPlayer() then
+            hook.Run("Horde_WardenWatchtower", self.Horde_Owner, self)
         end
+        self.Horde_NextThink = curTime
     end
 
-    if CurTime() >= self.Horde_NextShockAttack + self.Horde_ShockAttackInterval then
+    if self.Horde_EnableShockwave and curTime >= self.Horde_NextShockWave + self.Horde_ShockwaveInterval then
+        local dmg = DamageInfo()
+        dmg:SetAttacker(self.Horde_Owner)
+        dmg:SetInflictor(self)
+        dmg:SetDamageType(DMG_SHOCK)
+        dmg:SetDamage(50)
+        local e = EffectData()
+        e:SetOrigin(self:GetPos())
+        util.Effect("explosion_shock", e, true, true)
+        util.BlastDamageInfo(dmg, self:GetPos(), 160)
+        self.Horde_NextShockWave = curTime
+    end
+
+    if curTime >= self.Horde_NextShockAttack + self.Horde_ShockAttackInterval then
         local dmg = DamageInfo()
         dmg:SetAttacker(self.Horde_Owner)
         dmg:SetInflictor(self)
@@ -87,7 +102,9 @@ function ENT:Think()
             target:TakeDamageInfo(dmg)
             util.ParticleTracerEx("vortigaunt_beam", self:GetPos(), target:GetPos() + target:OBBCenter(), true, self:EntIndex(), -1)
             util.ParticleTracerEx("vortigaunt_beam_b", self:GetPos(), target:GetPos() + target:OBBCenter(), true, self:EntIndex(), -1)
-            self.Horde_NextShockAttack = CurTime()
+            self.Horde_NextShockAttack = curTime
         end
     end
+
+    self:NextThink(curTime + 0.1)
 end
