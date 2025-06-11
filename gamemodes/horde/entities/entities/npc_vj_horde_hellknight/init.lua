421d92082f9ce1903215aac0b7f687f2073c3527
func_brush <jacobamadolopez2006@gmail.com>
2025-06-11 11:31:03 -0700

    Hell Knight QOL

--- gamemodes/horde/entities/entities/npc_vj_horde_hellknight/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_hellknight/init.lua
@@ -1,14 +1,10 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/
+
 ENT.Model = {"models/zombie/zombie_fast02.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 6500
 ENT.HullType = HULL_HUMAN
----------------------------------------------------------------------------------------------------------------------------------------------
+
 ENT.VJ_NPC_Class = {"CLASS_ZOMBIE", "CLASS_XEN"} -- NPCs with the same class with be allied to each other
 ENT.BloodColor = "Red" -- The blood type, this will determine what it should use (decal, particle, etc.)
 ENT.HasMeleeAttack = true -- Should the SNPC have a melee attack?
@@ -34,11 +30,9 @@ ENT.LeapAttackDamage = 40
 ENT.LeapAttackDamageDistance = 100 -- How far does the damage go?
 ENT.FootStepTimeRun = 0.4 -- Next foot step sound when it is running
 ENT.FootStepTimeWalk = 0.6 -- Next foot step sound when it is walking
-	-- ====== Sound File Paths ====== --
--- Leave blank if you don't want any sounds to play
+
 ENT.SoundTbl_FootStep = {"npc/fast_zombie/foot1.wav","npc/fast_zombie/foot2.wav","npc/fast_zombie/foot3.wav","npc/fast_zombie/foot4.wav"}
-ENT.SoundTbl_Breath = {"npc/fast_zombie/breathe_loop1.wav"}
-ENT.SoundTbl_Alert = {"npc/fast_zombie/fz_alert_close1.wav","npc/fast_zombie/fz_alert_far1.wav"}
+ENT.SoundTbl_Alert = {"npc/fast_zombie/fz_alert_far1.wav"}
 ENT.SoundTbl_MeleeAttack = {"npc/fast_zombie/claw_strike1.wav","npc/fast_zombie/claw_strike2.wav","npc/fast_zombie/claw_strike3.wav"}
 ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
 ENT.SoundTbl_LeapAttackJump = {"npc/fast_zombie/fz_scream1.wav"}
@@ -47,7 +41,7 @@ ENT.SoundTbl_Pain = {"npc/fast_zombie/idle1.wav","npc/fast_zombie/idle2.wav","np
 ENT.SoundTbl_Death = {"npc/fast_zombie/wake1.wav"}
 
 ENT.GeneralSoundPitch1 = 70
-ENT.GeneralSoundPitch2 = 75
+ENT.GeneralSoundPitch2 = 70
 
 ENT.Raging = nil
 ENT.Raged = nil
@@ -56,16 +50,31 @@ ENT.DamageReceived = 0
 function ENT:Rage()
     if self.Raging or self.Raged then return end
     self.Raging = true
-    self:Ignite(99999999)
-    sound.Play("npc/fast_zombie/fz_frenzy1.wav", self:GetPos())
+    self:EmitSound("npc/fast_zombie/fz_frenzy1.wav",100)
     self:VJ_ACT_PLAYACTIVITY("BR2_Roar", true, 1.5, false)
     timer.Simple(1.5, function ()
-        if not IsValid(self) then return end
+        if not IsValid(self) then 
+            return 
+        end
         self.AnimTbl_Run = ACT_RUN
         self.HasLeapAttack = true
         self.Raged = true
         self.Raging = false
         self:SetColor(Color(125, 50, 50))
+			
+	local e = EffectData()
+        e:SetOrigin(self:GetPos())
+        e:SetScale(10)
+    	util.Effect("m2_flame_explosion", e, true, true)
+
+	local dmg = DamageInfo()
+    	dmg:SetInflictor(self)
+    	dmg:SetAttacker(self)
+    	dmg:SetDamageType(DMG_BURN)
+    	dmg:SetDamage(20)
+    	util.BlastDamageInfo(dmg, self:GetPos(), 250)
+	
+    	sound.Play("vj_fire/fireball_explode.wav", self:GetPos())
     end)
 end
 
@@ -78,21 +87,21 @@ function ENT:CustomOnTakeDamage_BeforeDamage(dmginfo, hitgroup)
 end
 
 function ENT:CustomOnInitialize()
-	self:SetCollisionBounds(Vector(13, 13, 50), Vector(-13, -13, 0))
+    self:SetCollisionBounds(Vector(13, 13, 50), Vector(-13, -13, 0))
     self:SetModelScale(1.75)
     self.HasLeapAttack = false
     self.AnimTbl_Run = ACT_WALK
     self:SetColor(Color(25, 25, 25))
-    self:Ignite(99999999)
-
+	
     local id = self:GetCreationID()
     timer.Remove("Horde_FlayerRage" .. id)
     timer.Create("Horde_FlayerRage" .. id, 10, 1, function ()
         if not IsValid(self) then return end
         self:Rage()
     end)
+	
     self:AddRelationship("npc_headcrab_poison D_LI 99")
-	self:AddRelationship("npc_headcrab_fast D_LI 99")
+    self:AddRelationship("npc_headcrab_fast D_LI 99")
 end
 
 function ENT:CustomOnMeleeAttack_AfterChecks(hitEnt, isProp)
@@ -102,6 +111,12 @@ function ENT:CustomOnMeleeAttack_AfterChecks(hitEnt, isProp)
         self:UnRage()
         hitEnt:Horde_AddDebuffBuildup(HORDE.Status_Ignite, 35, self)
     end
+end
+
+function ENT:CustomOnThink_AIEnabled()
+	if not self:IsOnFire() then
+		self:Ignite(99999)
+	end
 end
 
 function ENT:UnRage()
@@ -136,4 +151,14 @@ function ENT:CustomOnTakeDamage_AfterDamage(dmginfo, hitgroup)
     end
 end
 
+function ENT:CustomOnKilled(dmginfo,hitgroup)
+	self:StopSound("npc/fast_zombie/fz_frenzy1.wav")
+	self:Extinguish()
+end
+
+function ENT:CustomOnDeath_AfterCorpseSpawned(dmginfo,hitgroup,corpseEnt)
+	self.Corpse:Ignite(math.Rand(8,10),0)
+end
+
 VJ.AddNPC("Hell Knight","npc_vj_horde_hellknight", "Zombies")
+ENT.Immune_Fire = true
