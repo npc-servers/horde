6b90bbb5e445dcf9ca6473f3406426513b316ea1
func_brush <jacobamadolopez2006@gmail.com>
2025-06-11 11:58:02 -0700

    Reduced model scale of Blight

--- gamemodes/horde/entities/entities/npc_vj_horde_blight/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_blight/init.lua
@@ -38,10 +38,8 @@ function ENT:CustomOnInitialize()
     self:SetBodygroup(1,1)
     self:SetColor(Color(50, 50, 50))
     self.HeadHealth = self.HeadHealth * HORDE.Difficulty[HORDE.CurrentDifficulty].healthMultiplier
-    self:SetModelScale(1.5, 0)
-    self:ManipulateBoneScale(0, Vector(2,2,2))
-    self:ManipulateBoneScale(9, Vector(2,2,4))
-
+    self:SetModelScale(1.3, 0)
+    
     self:AddRelationship("npc_headcrab_poison D_LI 99")
 	self:AddRelationship("npc_headcrab_fast D_LI 99")
 end
