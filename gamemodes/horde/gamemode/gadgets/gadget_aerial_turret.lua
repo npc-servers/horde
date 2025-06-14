a3c9d1cc610166eb76f5b46427a455bc0cbee7ad
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-06-14 08:47:19 -0700

    Removed aerial turret code

--- gamemodes/horde/gamemode/gadgets/gadget_aerial_turret.lua
+++ gamemodes/horde/gamemode/gadgets/gadget_aerial_turret.lua
@@ -11,34 +11,5 @@ GADGET.Hooks = {}
 GADGET.Hooks.Horde_UseActiveGadget = function (ply)
     if CLIENT then return end
     if ply:Horde_GetGadget() ~= "gadget_aerial_turret" then return end
-    local ent = ents.Create("npc_vj_horde_smg_turret")
-    local pos = ply:GetPos()
-    local dir = (ply:GetEyeTrace().HitPos - pos)
-    dir:Normalize()
-    local drop_pos = pos + dir * 50
-    drop_pos.z = pos.z + 15
-    ent:SetPos(drop_pos)
-    ent:SetAngles(Angle(0, ply:GetAngles().y, 0))
-    ply:Horde_AddDropEntity(ent:GetClass(), ent)
-    ent:SetNWEntity("HordeOwner", ply)
-    ent:SetRenderMode(RENDERMODE_TRANSCOLOR)
-    ent:SetColor(Color(255,0,0,255))
-    ent:Spawn()
-    ent.Horde_Is_Mini_Sentry = true
-
-    ply:Horde_SetMinionCount(ply:Horde_GetMinionCount() + 1)
-
-    ent:CallOnRemove("Horde_EntityRemoved", function()
-        if ent:IsValid() and ply:IsValid() then
-            timer.Remove("Horde_MinionCollision" .. ent:GetCreationID())
-            ent:GetNWEntity("HordeOwner"):Horde_RemoveDropEntity(ent:GetClass(), ent:GetCreationID(), true)
-            ent:GetNWEntity("HordeOwner"):Horde_SyncEconomy()
-            ply:Horde_SetMinionCount(ply:Horde_GetMinionCount() - 1)
-        end
-    end)
-
-    timer.Simple(20, function()
-        if not ent:IsValid() then return end
-        ent:TakeDamage(ent:GetMaxHealth() + 1, Entity(0), Entity(0))
-    end)
+    -- Refer to gadget_turret_pack to restore this code
 end
