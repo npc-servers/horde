e55b34435ff3fbe82fc13cbb3d218c86e0cdabe9
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-06-14 12:40:05 -0700

    Reduced range, Reduced spread, Increase dmg

--- gamemodes/horde/entities/entities/npc_vj_horde_smg_turret/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_smg_turret/init.lua
@@ -28,13 +28,12 @@ ENT.HasRangeAttack = true -- Should the SNPC have a range attack?
 ENT.DisableDefaultRangeAttackCode = true -- When true, it won't spawn the range attack entity, allowing you to make your own
 ENT.DisableRangeAttackAnimation = true -- if true, it will disable the animation code
 ENT.RangeAttackReps = 1 -- How many times does it run the projectile code?
-ENT.NextRangeAttackTime = 0 -- How much time until it can use a range attack?
-ENT.NextAnyAttackTime_Range = 0 -- How much time until it can use any attack again? | Counted in Seconds
+ENT.NextRangeAttackTime = 0.07 -- How much time until it can use a range attack?
+ENT.NextAnyAttackTime_Range = 0.07 -- How much time until it can use any attack again? | Counted in Seconds
 -- ====== Distance Variables ====== --
 ENT.TimeUntilRangeAttackProjectileRelease = 0 -- How much time until the projectile code is ran?
-ENT.RangeDistance = 2000 -- This is how far away it can shoot
+ENT.RangeDistance = 1100 -- This is how far away it can shoot
 ENT.RangeToMeleeDistance = 0 -- How close does it have to be until it uses melee?
-ENT.NextRangeAttackTime = 0 -- How much time until it can use a range attack?
 ENT.HasFootStepSound = false
 ENT.SoundTbl_FootStep = {}
 ENT.CanTurnWhileStationary = false
@@ -136,11 +135,11 @@ function ENT:FireBullet( gunPos )
     bullet.Num = 1
     bullet.Src = gunPos
     bullet.Dir = ( self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter() + Vector( 0, 0, 10 ) ) - gunPos
-    bullet.Spread = Vector( 50, 50, 0 )
+    bullet.Spread = Vector( 44.5, 44.5, 0 )
     bullet.Tracer = 1
     bullet.TracerName = "Tracer"
     bullet.Force = 1
-    bullet.Damage = 10
+    bullet.Damage = 12
     bullet.AmmoType = "Pistol"
     self:FireBullets( bullet )
 end
