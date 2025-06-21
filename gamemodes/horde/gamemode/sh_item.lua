2ef030db69a697c4c19c95f9a1d51de73b5772a8
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-06-21 02:32:18 -0700

    Engineer: Style Inprove, Readability Improve

--- gamemodes/horde/gamemode/sh_item.lua
+++ gamemodes/horde/gamemode/sh_item.lua
@@ -868,32 +868,55 @@ function HORDE:GetDefaultItemsData()
         -- { Cremator = true },
         -- 100, -1, nil, nil, nil, nil, { HORDE.DMG_FIRE } )
 
+    HORDE:CreateItem( "Special", "Welder", "horde_welder", 100, 1,
+        "Engineering welder.\nDamages enemies and heals minions.",
+        { Engineer = true },
+        10, -1, nil, nil, nil, nil, { HORDE.DMG_BLAST }, nil, { "Engineer" } )
+    HORDE:CreateItem( "Special", "Manhack", "npc_manhack", 900, 3,
+        "Manhack that regenerates on death.\nManhack deals its health as damage to enemies.\nManhack dies on impact.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/npc_manhack.png", nil, nil, { HORDE.DMG_SLASH } )
+    HORDE:CreateItem( "Special", "Turret", "npc_turret_floor", 1000, 4,
+        "Combine Overwatch turret.\n\nUsed to guard chocke points and vital areas.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/npc_turret_floor.png", nil, nil, { HORDE.DMG_BALLISTIC } )
+    HORDE:CreateItem( "Special", "ShotgunTurret", "npc_vj_horde_shotgun_turret", 1250, 4,
+        "Combine shotgun turret.\n\nFires in a shotgun pattern.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/shotgun_turret.png", nil, nil, { HORDE.DMG_BALLISTIC } )
+    HORDE:CreateItem( "Special", "RocketTurret", "npc_vj_horde_rocket_turret", 1500, 4,
+        "Aperture Science rocket turret.\n\nShoots mini-missiles that deal Blast damage.\nCovers all angles.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/rocket_turret.png", nil, nil, { HORDE.DMG_BLAST } )
+    HORDE:CreateItem( "Special", "LaserTurret", "npc_vj_horde_laser_turret", 1500, 4,
+        "Aperture Science laser turret.\n\nFires tracing laser at the enemy.\nCovers all angles.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/laser_turret.png", { Engineer = 3 }, nil, { HORDE.DMG_BLAST } )
+    HORDE:CreateItem( "Special", "SniperTurret", "npc_vj_horde_sniper_turret", 1500, 4,
+        "Combine heavy sniper turret.\n\nCovers a long range and deals heavy damage, but with limited sight.\nAims for the head if possible.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/sniper_turret.png", { Engineer = 4 }, nil, { HORDE.DMG_BALLISTIC } )
+    HORDE:CreateItem( "Special", "Vortigaunt", "npc_vj_horde_vortigaunt", 1750, 5,
+        "Xen Vortigaunts that can conjure concentrated shock energy blasts.\nThe energy blasts have long range and deal splash damage.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 2 }, "items/npc_vortigaunt.png", { Engineer = 2 }, nil, { HORDE.DMG_LIGHTNING } )
+    HORDE:CreateItem( "Special", "CombatBot", "npc_vj_horde_combat_bot", 2750, 8,
+        "A resilient humanoid robot designed to engage enemies head-on.\nUses powerful melee attacks and ranged boulder attacks.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 1 }, "items/npc_combat_bot.png", { Engineer = 3 }, nil, { HORDE.DMG_BLUNT } )
+    HORDE:CreateItem( "Special", "Survivor", "npc_vj_horde_class_survivor", 1250, 4,
+        "A Survivor class human that acts as a multipurpose fighter. \nUses an AR15 rifle and grenades.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 3 }, "items/combine_merc.png", { Engineer = 4 }, nil, { HORDE.DMG_BALLISTIC } )
+    HORDE:CreateItem( "Special", "Assault", "npc_vj_horde_class_assault", 1750, 5,
+        "An Assault class human that uses automatic rifles. \nUses an M16 rifle with M203 UBGL and stun grenades.",
+        { Engineer = true },
+        10, -1, { type = HORDE.ENTITY_PROPERTY_DROP, x = 50, z = 15, yaw = 0, limit = 2 }, "items/combine_merc.png", { Engineer = 5 }, nil, { HORDE.DMG_BALLISTIC } )
 
-    HORDE:CreateItem("Special",    "Welder",         "horde_welder",         100,  1, "Engineering welder.\nDamages enemies and heals minions.",
-    {Engineer=true}, 10, -1, nil, nil, nil, nil, {HORDE.DMG_BLAST}, nil, {"Engineer"})
-    HORDE:CreateItem("Special",    "Manhack",        "npc_manhack",          900,  3, "Manhack that regenerates on death.\nManhack deals its health as damage to enemies.\nManhack dies on impact.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/npc_manhack.png", nil, nil, {HORDE.DMG_SLASH})
-    HORDE:CreateItem("Special",    "Turret",         "npc_turret_floor",    1000,  4, "Combine Overwatch turret.\n\nUsed to guard chocke points and vital areas.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/npc_turret_floor.png", nil, nil, {HORDE.DMG_BALLISTIC})
-    HORDE:CreateItem("Special",    "Shotgun Turret", "npc_vj_horde_shotgun_turret",   1250,  4, "Combine shotgun turret.\n\nFires in a shotgun pattern.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/shotgun_turret.png", nil, nil, {HORDE.DMG_BALLISTIC})
-    HORDE:CreateItem("Special",    "Rocket Turret",  "npc_vj_horde_rocket_turret",    1500,  4, "Aperture Science rocket turret.\n\nShoots mini-missiles that deal Blast damage.\nCovers all angles.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/rocket_turret.png", nil, nil, {HORDE.DMG_BLAST})
-    HORDE:CreateItem("Special",    "Laser Turret",  "npc_vj_horde_laser_turret",    1500,  4, "Aperture Science laser turret.\n\nFires tracing laser at the enemy.\nCovers all angles.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/laser_turret.png", {Engineer=3}, nil, {HORDE.DMG_BLAST})
-    HORDE:CreateItem("Special",    "Sniper Turret",  "npc_vj_horde_sniper_turret",   1500,  4, "Combine heavy sniper turret.\n\nCovers a long range and deals heavy damage, but with limited sight.\nAims for the head if possible.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/sniper_turret.png", {Engineer=4}, nil, {HORDE.DMG_BALLISTIC})
-    HORDE:CreateItem("Special",    "Vortigaunt",     "npc_vj_horde_vortigaunt",  1750,  5, "Xen Vortigaunts that can conjure concentrated shock energy blasts.\nThe energy blasts have long range and deal splash damage.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=2}, "items/npc_vortigaunt.png", {Engineer=2}, nil, {HORDE.DMG_LIGHTNING})
-    HORDE:CreateItem("Special",    "Combat Bot",     "npc_vj_horde_combat_bot",   2750, 8, "A resilient humanoid robot designed to engage enemies head-on.\nUses powerful melee attacks and ranged boulder attacks.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=1}, "items/npc_combat_bot.png", {Engineer=3}, nil, {HORDE.DMG_BLUNT})
-    HORDE:CreateItem("Special",    "Survivor",     "npc_vj_horde_class_survivor",   1250, 4, "A Survivor class human that acts as a multipurpose fighter. \nUses an AR15 rifle and grenades.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=3}, "items/combine_merc.png", {Engineer=4}, nil, {HORDE.DMG_BALLISTIC})
-    HORDE:CreateItem("Special",    "Assault",     "npc_vj_horde_class_assault",   1750, 5, "An Assault class human that uses automatic rifles. \nUses an M16 rifle with M203 UBGL and stun grenades.",
-    {Engineer=true}, 10, -1, {type=HORDE.ENTITY_PROPERTY_DROP, x=50, z=15, yaw=0, limit=2}, "items/combine_merc.png", {Engineer=5}, nil, {HORDE.DMG_BALLISTIC})
-    
-    HORDE:CreateItem("Special",    "Hivehand",       "horde_hivehand",       2000,  5, "Organic weapon used by Xen soldiers.\nHas infinite ammo.\nPrimary fire generates homing ricocheting shots.\nSecondary fire rapidly unloads the entire weapon.",
-    {Engineer=true}, 2, -1, nil, nil, {Engineer=4}, nil, {HORDE.DMG_BALLISTIC})
+    HORDE:CreateItem( "Special", "Hivehand", "horde_hivehand", 2000, 5,
+        "Organic weapon used by Xen soldiers.\nHas infinite ammo.\nPrimary fire generates homing ricocheting shots.\nSecondary fire rapidly unloads the entire weapon.",
+        { Engineer = true },
+        2, -1, nil, nil, { Engineer = 4 }, nil, { HORDE.DMG_BALLISTIC } )
     HORDE:CreateItem("Special",    "Spore Launcher", "horde_spore_launcher", 1750,  6, "Improvised biological weapon.\nShoots out acidic projectiles that explodes after a short delay.\nHeals players and damages enemies.",
     {Medic=true, Survivor=true}, 10, -1, nil, nil, {Medic=2, Survivor=2}, nil, {HORDE.DMG_POISON})
     HORDE:CreateItem("Special",    "M2 Health Thrower", "horde_healingthrower", 3250,  7, "M2-2 Health Thrower.\nAn American man-portable backpack flamethrower converted to heal stuff to death.\nsmells faintly of lime and mint (who hurt you)",
