ee5a21e854e417d24fe629de3e98fe2d96d2f5d3
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-07-05 18:00:33 -0700

    5%>10% hp leech, kills armor leech 2 > 4

--- gamemodes/horde/gamemode/perks/cyborg_ninja/cyborg_ninja_cybernetic_arm.lua
+++ gamemodes/horde/gamemode/perks/cyborg_ninja/cyborg_ninja_cybernetic_arm.lua
@@ -1,25 +1,31 @@
 PERK.PrintName = "Cybernetic Arm"
-PERK.Description = "Leech up to {1} health upon dealing melee damage. \nGain immunity to Bleeding. \nKills leech {2} armor while not in Blade Mode or Ripper Mode."
+PERK.Description = [[
+Leech up to {1} health upon dealing melee damage.
+Gain immunity to Bleeding.
+Kills leech {2} armor while not in Blade Mode or Ripper Mode.]]
 PERK.Icon = "materials/perks/berserk.png"
 PERK.Params = {
-    [1] = { value = 0.05, percent = true },
-    [2] = { value = 2 },
-    [3] = { value = 1 },
+    [1] = { value = hpleech, percent = true },
+    [2] = { value = 4 },
 }
-
 PERK.Hooks = {}
 
-PERK.Hooks.Horde_OnPlayerDamagePost = function( ply, _, _, _, dmginfo )
-    if ply:Horde_GetPerk( "cyborg_ninja_cybernetic_arm" ) and HORDE:IsMeleeDamage( dmginfo ) then
-        local leech = math.min( 5, dmginfo:GetDamage() * 0.10 )
-        HORDE:SelfHeal( ply, leech )
+PERK.Hooks.Horde_OnSetPerk = function( ply, perk )
+    if SERVER and perk == "cyborg_ninja_cybernetic_arm" then
+        ply:Horde_SetImmuneToDebuff( HORDE.Status_Bleeding, true )
     end
 end
 
-PERK.Hooks.Horde_OnPlayerDebuffApply = function( ply, debuff, bonus )
-    if ply:Horde_GetPerk( "cyborg_ninja_cybernetic_arm" ) and debuff == HORDE.Status_Bleeding then
-        bonus.apply = 0
-        return true
+PERK.Hooks.Horde_OnUnsetPerk = function( ply, perk )
+    if SERVER and perk == "cyborg_ninja_cybernetic_arm" then
+        ply:Horde_SetImmuneToDebuff( HORDE.Status_Bleeding, false )
+    end
+end
+
+PERK.Hooks.Horde_OnPlayerDamagePost = function( ply, _, _, _, dmginfo )
+    if ply:Horde_GetPerk( "cyborg_ninja_cybernetic_arm" ) and HORDE:IsMeleeDamage( dmginfo ) then
+        local leech = math.min( 10, dmginfo:GetDamage() * 0.10 )
+        HORDE:SelfHeal( ply, leech )
     end
 end
 
@@ -27,5 +33,5 @@ PERK.Hooks.Horde_OnEnemyKilled = function( _, killer )
     if not killer:Horde_GetPerk( "cyborg_ninja_cybernetic_arm" ) then return end
     if killer.Horde_In_Frenzy_Mode then return end
     if killer.Horde_Ripper_Mode then return end
-    killer:SetArmor( math.min( killer:GetMaxArmor(), killer:Armor() + 2 ) )
+    killer:SetArmor( math.min( killer:GetMaxArmor(), killer:Armor() + 4 ) )
 end
