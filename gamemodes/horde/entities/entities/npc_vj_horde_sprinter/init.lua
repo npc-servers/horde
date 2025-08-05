a32c031ca5feb3390c035947063dd4f0ed95bcff
func_brush <jacobamadolopez2006@gmail.com>
2025-08-05 08:08:19 -0700

    Small update to Sprinter

--- gamemodes/horde/entities/entities/npc_vj_horde_sprinter/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_sprinter/init.lua
@@ -1,33 +1,33 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
 
-ENT.Model = {"models/blacktea/zpszombie1.mdl","models/blacktea/zpszombie2.mdl","models/blacktea/zpszombie3.mdl","models/blacktea/zpszombie5.mdl"}
+ENT.Model = {"models/vj_zombies/panic_carrier.mdl"}
 ENT.StartHealth = 90
 ENT.VJ_NPC_Class = {"CLASS_ZOMBIE","CLASS_XEN"}
 ENT.BloodColor = "Red"
 ENT.MeleeAttackDamage = 15
-ENT.AnimTbl_MeleeAttack = {"vjseq_weapon_attack1","vjseq_weapon_attack2"}
-ENT.MeleeAttackDistance = 32
-ENT.MeleeAttackDamageDistance = 64
+ENT.AnimTbl_MeleeAttack = {"vjges_attack1","vjges_attack2","vjges_attack3"}
+ENT.TimeUntilMeleeAttackDamage = 0.4
+ENT.NextMeleeAttackTime = 1
 ENT.SlowPlayerOnMeleeAttack = true
 ENT.FootStepTimeRun = 0.5
 ENT.DisableFootStepOnWalk = true
 ENT.AlertSounds_OnlyOnce = true
+ENT.HasExtraMeleeAttackSounds = true
 ENT.HasImpactSounds = false
 
 ENT.SoundTbl_FootStep = {"npc/zombie/foot1.wav","npc/zombie/foot2.wav","npc/zombie/foot3.wav"}
-ENT.SoundTbl_Idle = {")zsszombie/zombie_idle1.wav",")zsszombie/zombie_idle2.wav",")zsszombie/zombie_idle3.wav",")zsszombie/zombie_idle4.wav",")zsszombie/zombie_idle5.wav","zsszombie/zombie_idle6.wav"}
-ENT.SoundTbl_Alert = {")zsszombie/zombie_alert1.wav",")zsszombie/zombie_alert2.wav","zsszombie/zombie_alert3.wav","zsszombie/zombie_alert4.wav"}
-ENT.SoundTbl_MeleeAttack = {"zsszombie/zombie_attack_1.wav","zsszombie/zombie_attack_2.wav",")zsszombie/zombie_attack_3.wav","zsszombie/zombie_attack_4.wav","zsszombie/zombie_attack_5.wav","zsszombie/zombie_attack_6.wav"}
-ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
-ENT.SoundTbl_Pain = {"zsszombie/zombie_pain1.wav","zsszombie/zombie_pain2.wav","zsszombie/zombie_pain3.wav","zsszombie/zombie_pain4.wav","zsszombie/zombie_pain5.wav","zsszombie/zombie_pain6.wav","zsszombie/zombie_pain7.wav","zsszombie/zombie_pain8.wav"}
-ENT.SoundTbl_Death = {"zsszombie/zombie_die1.wav",")zsszombie/zombie_die2.wav","zsszombie/zombie_die3.wav",")zsszombie/zombie_die4.wav",")zsszombie/zombie_die5.wav",")zsszombie/zombie_die6.wav"}
+ENT.SoundTbl_Idle = {")vj_zombies/panic/male/zmale_taunt1.wav",")vj_zombies/panic/male/zmale_taunt2.wav",")vj_zombies/panic/male/zmale_taunt3.wav",")vj_zombies/panic/male/zmale_taunt4.wav",")vj_zombies/panic/male/zmale_taunt5.wav",")vj_zombies/panic/male/zmale_taunt6.wav",")vj_zombies/panic/male/zmale_taunt7.wav",")vj_zombies/panic/male/zmale_taunt8.wav",")vj_zombies/panic/male/zmale_taunt9.wav",")vj_zombies/panic/male/zmale_taunt10.wav"}
+ENT.SoundTbl_Alert = {")vj_zombies/panic/male/zmale_berserk-01.wav",")vj_zombies/panic/male/zmale_berserk-02.wav",")vj_zombies/panic/male/zmale_berserk-03.wav",")vj_zombies/panic/male/zmale_berserk-04.wav",")vj_zombies/panic/male/zmale_berserk-05.wav",")vj_zombies/panic/male/zmale_berserk-06.wav",")vj_zombies/panic/male/zmale_berserk-07.wav",")vj_zombies/panic/male/zmale_berserk-08.wav"}
+ENT.SoundTbl_BeforeMeleeAttack = {")vj_zombies/panic/male/zmale_attack1.wav",")vj_zombies/panic/male/zmale_attack2.wav",")vj_zombies/panic/male/zmale_attack3.wav",")vj_zombies/panic/male/zmale_attack4.wav",")vj_zombies/panic/male/zmale_attack5.wav",")vj_zombies/panic/male/zmale_attack6.wav",")vj_zombies/panic/male/zmale_attack7.wav"}
+ENT.SoundTbl_MeleeAttackMiss = {"vj_zombies/slow/miss1.wav","vj_zombies/slow/miss2.wav","vj_zombies/slow/miss3.wav","vj_zombies/slow/miss4.wav"}
+ENT.SoundTbl_Pain = {")vj_zombies/panic/male/zmale_pain1.wav",")vj_zombies/panic/male/zmale_pain2.wav",")vj_zombies/panic/male/zmale_pain3.wav",")vj_zombies/panic/male/zmale_pain4.wav",")vj_zombies/panic/male/zmale_pain5.wav",")vj_zombies/panic/male/zmale_pain6.wav"}
+ENT.SoundTbl_Death = {")vj_zombies/panic/male/zmale_death1.wav",")vj_zombies/panic/male/zmale_death2.wav",")vj_zombies/panic/male/zmale_death3.wav",")vj_zombies/panic/male/zmale_death4.wav",")vj_zombies/panic/male/zmale_death5.wav",")vj_zombies/panic/male/zmale_death6.wav"}
 
-ENT.GeneralSoundPitch1 = 60
-ENT.GeneralSoundPitch2 = 60
-ENT.MeleeAttackSoundPitch = VJ_Set(100, 100)
+ENT.GeneralSoundPitch1 = 100
 
 function ENT:CustomOnInitialize()
 	self:AddRelationship("npc_headcrab_fast D_LI 99")
 	self:AddRelationship("npc_headcrab_poison D_LI 99")
+	self:SetSkin(math.random(0,1))
 end
