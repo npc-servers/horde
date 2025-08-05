2a9de8c46091d06f6ccfddaffd155c18675d4b76
func_brush <jacobamadolopez2006@gmail.com>
2025-08-05 08:05:10 -0700

    Small update to Crawler

--- gamemodes/horde/entities/entities/npc_vj_horde_crawler/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_crawler/init.lua
@@ -1,17 +1,14 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
 
-ENT.Model = {"models/zombie/zm_fast.mdl"}
+ENT.Model = {"models/vj_zombies/fast4.mdl"}
 ENT.StartHealth = 30
 ENT.VJ_NPC_Class = {"CLASS_ZOMBIE","CLASS_XEN"}
 ENT.BloodColor = "Red"
 ENT.MeleeAttackDamage = 5
 ENT.AnimTbl_MeleeAttack = {"vjseq_melee"}
-ENT.MeleeAttackDistance = 40
-ENT.MeleeAttackDamageDistance = 60
-ENT.TimeUntilMeleeAttackDamage = 0.25
+ENT.TimeUntilMeleeAttackDamage = 0.2
 ENT.NextMeleeAttackTime = 1
-ENT.MeleeAttackExtraTimers = {0.5}
 ENT.FootStepTimeRun = 0.25
 ENT.DisableFootStepOnWalk = true
 ENT.AlertSounds_OnlyOnce = true
@@ -19,12 +16,12 @@ ENT.HasExtraMeleeAttackSounds = true
 ENT.HasImpactSounds = false
 
 ENT.SoundTbl_FootStep = {"npc/fast_zombie/foot1.wav","npc/fast_zombie/foot2.wav","npc/fast_zombie/foot3.wav","npc/fast_zombie/foot4.wav"}
-ENT.SoundTbl_Idle = {"vfastzombie/fzombie_idle1.wav","vfastzombie/fzombie_idle2.wav","vfastzombie/fzombie_idle3.wav",")vfastzombie/fzombie_idle4.wav",")vfastzombie/fzombie_idle5.wav"}
-ENT.SoundTbl_Alert = {"vfastzombie/fzombie_alert1.wav","vfastzombie/fzombie_alert2.wav","vfastzombie/fzombie_alert3.wav"}
-ENT.SoundTbl_BeforeMeleeAttack = {"vfastzombie/attack1.wav","vfastzombie/attack2.wav","vfastzombie/attack3.wav"}
-ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
-ENT.SoundTbl_Pain = {"vfastzombie/fzombie_pain1.wav","vfastzombie/fzombie_pain2.wav","vfastzombie/fzombie_pain3.wav"}
-ENT.SoundTbl_Death = {"vfastzombie/fzombie_die1.wav","vfastzombie/fzombie_die2.wav"}
+ENT.SoundTbl_Idle = {")vj_zombies/fast/fzombie_idle1.wav",")vj_zombies/fast/fzombie_idle2.wav",")vj_zombies/fast/fzombie_idle3.wav",")vj_zombies/fast/fzombie_idle4.wav",")vj_zombies/fast/fzombie_idle5.wav"}
+ENT.SoundTbl_Alert = {")vj_zombies/fast/fzombie_alert1.wav",")vj_zombies/fast/fzombie_alert2.wav",")vj_zombies/fast/fzombie_alert3.wav"}
+ENT.SoundTbl_BeforeMeleeAttack = {")vj_zombies/fast/attack1.wav",")vj_zombies/fast/attack2.wav",")vj_zombies/fast/attack3.wav"}
+ENT.SoundTbl_MeleeAttackMiss = {"vj_zombies/slow/miss1.wav","vj_zombies/slow/miss2.wav","vj_zombies/slow/miss3.wav","vj_zombies/slow/miss4.wav"}
+ENT.SoundTbl_Pain = {")vj_zombies/fast/fzombie_pain1.wav",")vj_zombies/fast/fzombie_pain2.wav",")vj_zombies/fast/fzombie_pain3.wav"}
+ENT.SoundTbl_Death = {")vj_zombies/fast/fzombie_die1.wav",")vj_zombies/fast/fzombie_die2.wav"}
 
 ENT.GeneralSoundPitch1 = 100
 
