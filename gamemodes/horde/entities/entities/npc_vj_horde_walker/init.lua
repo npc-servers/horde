8220e14b31a3e9225b64c25158bc1257e43fd0a1
func_brush <jacobamadolopez2006@gmail.com>
2025-08-05 08:01:12 -0700

    Small update to Walker

--- gamemodes/horde/entities/entities/npc_vj_horde_walker/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_walker/init.lua
@@ -1,14 +1,14 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
 
-ENT.Model = {"models/zombie/zclassic_01.mdl","models/zombie/zclassic_02.mdl","models/zombie/zclassic_03.mdl","models/zombie/zclassic_04.mdl","models/zombie/zclassic_05.mdl","models/zombie/zclassic_06.mdl","models/zombie/zclassic_07.mdl","models/zombie/zclassic_08.mdl","models/zombie/zclassic_09.mdl","models/zombie/zclassic_10.mdl","models/zombie/zclassic_11.mdl","models/zombie/zclassic_12.mdl"}
+ENT.Model = {"models/vj_zombies/slow2.mdl","models/vj_zombies/slow3.mdl","models/vj_zombies/slow5.mdl","models/vj_zombies/slow7.mdl","models/vj_zombies/slow8.mdl"}
 ENT.StartHealth = 75
 ENT.VJ_NPC_Class = {"CLASS_ZOMBIE","CLASS_XEN"}
 ENT.BloodColor = "Red"
+ENT.CanFlinch = 1 -- 0 = Don't flinch | 1 = Flinch at any damage | 2 = Flinch only from certain damages
+ENT.AnimTbl_Flinch = {"vjges_flinch_chest"}
 ENT.MeleeAttackDamage = 20
 ENT.AnimTbl_MeleeAttack = {"vjseq_attacka","vjseq_attackb","vjseq_attackc","vjseq_attackd","vjseq_attacke","vjseq_attackf"}
-ENT.MeleeAttackDistance = 40
-ENT.MeleeAttackDamageDistance = 60
 ENT.TimeUntilMeleeAttackDamage = 1
 ENT.SlowPlayerOnMeleeAttack = true
 ENT.FootStepTimeRun = 1
@@ -17,12 +17,12 @@ ENT.AlertSounds_OnlyOnce = true
 ENT.HasImpactSounds = false
 
 ENT.SoundTbl_FootStep = {"npc/zombie/foot1.wav","npc/zombie/foot2.wav","npc/zombie/foot3.wav","npc/zombie/foot_slide1.wav","npc/zombie/foot_slide2.wav","npc/zombie/foot_slide3.wav"}
-ENT.SoundTbl_Idle = {")zsszombie/zombie_idle1.wav",")zsszombie/zombie_idle2.wav",")zsszombie/zombie_idle3.wav",")zsszombie/zombie_idle4.wav",")zsszombie/zombie_idle5.wav","zsszombie/zombie_idle6.wav"}
-ENT.SoundTbl_Alert = {")zsszombie/zombie_alert1.wav",")zsszombie/zombie_alert2.wav","zsszombie/zombie_alert3.wav","zsszombie/zombie_alert4.wav"}
-ENT.SoundTbl_MeleeAttack = {"zsszombie/zombie_attack_1.wav","zsszombie/zombie_attack_2.wav",")zsszombie/zombie_attack_3.wav","zsszombie/zombie_attack_4.wav","zsszombie/zombie_attack_5.wav","zsszombie/zombie_attack_6.wav"}
-ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
-ENT.SoundTbl_Pain = {"zsszombie/zombie_pain1.wav","zsszombie/zombie_pain2.wav","zsszombie/zombie_pain3.wav","zsszombie/zombie_pain4.wav","zsszombie/zombie_pain5.wav","zsszombie/zombie_pain6.wav","zsszombie/zombie_pain7.wav","zsszombie/zombie_pain8.wav"}
-ENT.SoundTbl_Death = {"zsszombie/zombie_die1.wav",")zsszombie/zombie_die2.wav","zsszombie/zombie_die3.wav",")zsszombie/zombie_die4.wav",")zsszombie/zombie_die5.wav",")zsszombie/zombie_die6.wav"}
+ENT.SoundTbl_Idle = {")vj_zombies/slow/zombie_idle1.wav",")vj_zombies/slow/zombie_idle2.wav",")vj_zombies/slow/zombie_idle3.wav",")vj_zombies/slow/zombie_idle4.wav",")vj_zombies/slow/zombie_idle5.wav",")vj_zombies/slow/zombie_idle6.wav"}
+ENT.SoundTbl_Alert = {")vj_zombies/slow/zombie_alert1.wav",")vj_zombies/slow/zombie_alert2.wav",")vj_zombies/slow/zombie_alert3.wav",")vj_zombies/slow/zombie_alert4.wav"}
+ENT.SoundTbl_MeleeAttack = {")vj_zombies/slow/zombie_attack_1.wav",")vj_zombies/slow/zombie_attack_2.wav",")vj_zombies/slow/zombie_attack_3.wav",")vj_zombies/slow/zombie_attack_4.wav",")vj_zombies/slow/zombie_attack_5.wav",")vj_zombies/slow/zombie_attack_6.wav"}
+ENT.SoundTbl_MeleeAttackMiss = {"vj_zombies/slow/miss1.wav","vj_zombies/slow/miss2.wav","vj_zombies/slow/miss3.wav","vj_zombies/slow/miss4.wav"}
+ENT.SoundTbl_Pain = {")vj_zombies/slow/zombie_pain1.wav",")vj_zombies/slow/zombie_pain2.wav",")vj_zombies/slow/zombie_pain3.wav",")vj_zombies/slow/zombie_pain4.wav",")vj_zombies/slow/zombie_pain5.wav",")vj_zombies/slow/zombie_pain6.wav",")vj_zombies/slow/zombie_pain7.wav",")vj_zombies/slow/zombie_pain8.wav"}
+ENT.SoundTbl_Death = {")vj_zombies/slow/zombie_die1.wav",")vj_zombies/slow/zombie_die2.wav",")vj_zombies/slow/zombie_die3.wav",")vj_zombies/slow/zombie_die4.wav",")vj_zombies/slow/zombie_die5.wav",")vj_zombies/slow/zombie_die6.wav"}
 
 ENT.GeneralSoundPitch1 = 100
 
@@ -34,4 +34,17 @@ function ENT:CustomOnThink_AIEnabled()
 	self.AnimTbl_IdleStand = {self:IsOnFire() and ACT_IDLE_ON_FIRE or ACT_IDLE}
 	self.AnimTbl_Walk = {self:IsOnFire() and ACT_WALK_ON_FIRE or ACT_WALK}
 	self.AnimTbl_Run = {self:IsOnFire() and ACT_WALK_ON_FIRE or ACT_RUN}
+end
+function ENT:CustomOnFlinch_BeforeFlinch(dmginfo, hitgroup)
+	if hitgroup == HITGROUP_LEFTARM then
+		self.AnimTbl_Flinch = {"vjges_flinch_leftarm"}
+	elseif hitgroup == HITGROUP_RIGHTARM then
+		self.AnimTbl_Flinch = {"vjges_flinch_rightarm"}
+	elseif hitgroup == HITGROUP_LEFTLEG then
+		self.AnimTbl_Flinch = {"vjseq_flinch_leftleg"}
+	elseif hitgroup == HITGROUP_RIGHTLEG then
+		self.AnimTbl_Flinch = {"vjseq_flinch_rightleg"}
+	elseif hitgroup == HITGROUP_HEAD then
+		self.AnimTbl_Flinch = {"vjges_flinch_head"}
+	end
 end
