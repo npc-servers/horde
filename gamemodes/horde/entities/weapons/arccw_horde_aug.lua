362c944b50b03df0e49f67329203854fe06a0e12
func_brush <jacobamadolopez2006@gmail.com>
2025-08-11 16:39:18 -0700

    Add files via upload

--- gamemodes/horde/entities/weapons/arccw_horde_aug.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_aug.lua
@@ -58,7 +58,7 @@ SWEP.Primary.Ammo = "smg1"
 
 SWEP.ShootVol = 75
 
-SWEP.ShootSound = {")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav"} -- When using stereo .wav files, add ) to the beginning to make it directional. - func_brush
+SWEP.ShootSound = {")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav",")arccw_go/aug/aug_01.wav"}
 SWEP.ShootSoundSilenced = ")arccw_go/m4a1/m4a1_silencer_01.wav"
 SWEP.DistantShootSound = ")arccw_go/aug/aug_distant.wav"
 
@@ -346,35 +346,7 @@ sound.Add({
     sound = "arccw_go/aug/aug_cliphit.wav"
 })
 
---[[ OLD CODE
-
-if not ArcCWInstalled then return end
-if CLIENT then
-    SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_go_aug")
-    killicon.Add("arccw_horde_aug", "arccw/weaponicons/arccw_go_aug", Color(0, 0, 0, 255))
-end
-SWEP.Base = "arccw_go_aug"
-SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - Horde" -- edit this if you like
-SWEP.AdminOnly = false
-
-SWEP.PrintName = "AUG A3"
-SWEP.Slot = 2
-
-SWEP.ViewModel = "models/weapons/arccw_go/v_rif_aug.mdl"
-SWEP.WorldModel = "models/weapons/arccw_go/v_rif_aug.mdl"
-
-SWEP.Damage = 63
-SWEP.DamageMin = 47 -- damage done at maximum range
-
-SWEP.RecoilPunch = 0
-
-SWEP.Delay = 60 / 700 -- 60 / RPM.
-SWEP.ShootVol = 75 -- volume of shoot sound
-
-]]
-
-function SWEP:DoShootSound(sndoverride, dsndoverride, voloverride, pitchoverride) -- For edited shooting sounds.
+function SWEP:DoShootSound(sndoverride, dsndoverride, voloverride, pitchoverride)
     local fsound = self.ShootSound
     local suppressed = self:GetBuff_Override("Silencer")
 
@@ -438,7 +410,7 @@ function SWEP:DoShootSound(sndoverride, dsndoverride, voloverride, pitchoverride
 
     if distancesound then self:MyEmitSound(distancesound, 140, pitch, 0.5, CHAN_WEAPON) end
 
-    if fsound then self:MyEmitSound(fsound, volume, pitch, 1, CHAN_STATIC) end -- Only use ONE instance of CHAN_STATIC for shooting sounds, any more will cause bugs. - func_brush
+    if fsound then self:MyEmitSound(fsound, volume, pitch, 1, CHAN_STATIC) end
 
     local data = {
         sound   = fsound,
