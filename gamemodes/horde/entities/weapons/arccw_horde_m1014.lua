a0b12679eed602777f835fbb5c47934344a2c675
func_brush <jacobamadolopez2006@gmail.com>
2025-08-19 10:46:01 -0700

    CS:GO Shotgun Changes (#325)
    * CS:GO Shotgun Changes
    
    * extra tweaks to shotguns

--- gamemodes/horde/entities/weapons/arccw_horde_m1014.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_m1014.lua
@@ -3,23 +3,22 @@ if CLIENT then
     SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_go_m1014")
     killicon.Add("arccw_horde_m1014", "arccw/weaponicons/arccw_go_m1014", Color(0, 0, 0, 255))
 end
+
 SWEP.Base = "arccw_go_m1014"
-SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - GSO (SGs)" -- edit this if you like
+
+SWEP.Spawnable = true
+SWEP.Category = "ArcCW - Horde"
 SWEP.AdminOnly = false
 
 SWEP.PrintName = "M1014"
-SWEP.Slot = 2
 
 SWEP.ViewModel = "models/weapons/arccw_go/v_shot_m1014.mdl"
 SWEP.WorldModel = "models/weapons/arccw_go/v_shot_m1014.mdl"
 
-
 SWEP.Damage = 26
-SWEP.DamageMin = 13 -- damage done at maximum range
-SWEP.Recoil = 4
+
+SWEP.Recoil = 3
 SWEP.RecoilSide = 2
-SWEP.RecoilRise = 0.1
 SWEP.RecoilPunch = 0
 
 SWEP.Firemodes = {
@@ -34,12 +33,95 @@ SWEP.Firemodes = {
     }
 }
 
+SWEP.ShootVol = 75
 
-SWEP.AccuracyMOA = 75 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.ShootSound = ")arccw_go/xm1014/xm1014-1.wav"
+SWEP.ShootSoundSilenced = ")arccw_go/m590_suppressed_fp.wav"
+SWEP.DistantShootSound = ")arccw_go/xm1014/xm1014-1-distant.wav"
 
+SWEP.MeleeSwingSound = "weapons/arccw/melee_lift.wav"
+SWEP.MeleeMissSound = "weapons/arccw/melee_miss.wav"
+SWEP.MeleeHitSound = "weapons/arccw/melee_hitworld.wav"
+SWEP.MeleeHitNPCSound = "weapons/arccw/melee_hitbody.wav"
 
-SWEP.ShootVol = 75 -- volume of shoot sound
+SWEP.ActivePos = Vector(0, -2, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
 
-SWEP.SpeedMult = 1
+function SWEP:Hook_TranslateAnimation(anim)
+    return false
+end
 
-SWEP.SightTime = 0.2
+function SWEP:DoShootSound(sndoverride, dsndoverride, voloverride, pitchoverride)
+    local fsound = self.ShootSound
+    local suppressed = self:GetBuff_Override("Silencer")
+
+    if suppressed then
+        fsound = self.ShootSoundSilenced
+    end
+
+    local firstsound = self.FirstShootSound
+
+    if self:GetBurstCount() == 1 and firstsound then
+        fsound = firstsound
+
+        local firstsil = self.FirstShootSoundSilenced
+
+        if suppressed then
+            fsound = firstsil and firstsil or self.ShootSoundSilenced
+        end
+    end
+
+    local lastsound = self.LastShootSound
+
+    local clip = self:Clip1()
+
+    if clip == 1 and lastsound then
+        fsound = lastsound
+
+        local lastsil = self.LastShootSoundSilenced
+
+        if suppressed then
+            fsound = lastsil and lastsil or self.ShootSoundSilenced
+        end
+    end
+
+    fsound = self:GetBuff_Hook("Hook_GetShootSound", fsound)
+
+    local distancesound = self.DistantShootSound
+
+    if suppressed then
+        distancesound = self.DistantShootSoundSilenced
+    end
+
+    distancesound = self:GetBuff_Hook("Hook_GetDistantShootSound", distancesound)
+
+    local spv = self.ShootPitchVariation
+    local volume = self.ShootVol
+    local pitch  = self.ShootPitch * math.Rand(1 - spv, 1 + spv) * self:GetBuff_Mult("Mult_ShootPitch")
+
+    local v = ArcCW.ConVars["weakensounds"]:GetFloat()
+
+    volume = volume - v
+
+    volume = volume * self:GetBuff_Mult("Mult_ShootVol")
+
+    volume = math.Clamp(volume, 50, 140)
+    pitch  = math.Clamp(pitch, 0, 255)
+
+    if    sndoverride        then    fsound    = sndoverride end
+    if    dsndoverride    then    distancesound = dsndoverride end
+    if    voloverride        then    volume    = voloverride end
+    if    pitchoverride    then    pitch    = pitchoverride end
+
+    if distancesound then self:MyEmitSound(distancesound, 140, pitch, 0.25, CHAN_WEAPON) end
+
+    if fsound then self:MyEmitSound(fsound, volume, pitch, 1, CHAN_STATIC) end
+
+    local data = {
+        sound   = fsound,
+        volume  = volume,
+        pitch   = pitch,
+    }
+
+    self:GetBuff_Hook("Hook_AddShootSound", data)
+end
--- gamemodes/horde/entities/weapons/arccw_horde_mag7.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_mag7.lua
@@ -3,24 +3,148 @@ if CLIENT then
     SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_go_mag7")
     killicon.Add("arccw_horde_mag7", "arccw/weaponicons/arccw_go_mag7", Color(0, 0, 0, 255))
 end
+
 SWEP.Base = "arccw_go_mag7"
-SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - Horde" -- edit this if you like
+
+SWEP.Spawnable = true
+SWEP.Category = "ArcCW - Horde"
 SWEP.AdminOnly = false
 
 SWEP.PrintName = "MAG-7"
-
-SWEP.Slot = 2
-
-SWEP.UseHands = true
 
 SWEP.ViewModel = "models/weapons/arccw_go/v_shot_mag7.mdl"
 SWEP.WorldModel = "models/weapons/arccw_go/v_shot_mag7.mdl"
 
 SWEP.Damage = 17
 
-SWEP.Range = 45 -- in METRES
+SWEP.NoLastCycle = true
+
+SWEP.Recoil = 2
+SWEP.RecoilSide = 2
 SWEP.RecoilPunch = 0
 
+SWEP.ShootVol = 75
 
-SWEP.AccuracyMOA = 65 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.ShootSound = {")arccw_go/mag7/mag7_01.wav",")arccw_go/mag7/mag7_02.wav"}
+SWEP.ShootSoundSilenced = ")arccw_go/m590_suppressed_fp.wav"
+SWEP.DistantShootSound = {")arccw_go/mag7/mag7_distant_01.wav","arccw_go/mag7/mag7_distant_02.wav"}
+
+SWEP.MeleeSwingSound = "weapons/arccw/melee_lift.wav"
+SWEP.MeleeMissSound = "weapons/arccw/melee_miss.wav"
+SWEP.MeleeHitSound = "weapons/arccw/melee_hitworld.wav"
+SWEP.MeleeHitNPCSound = "weapons/arccw/melee_hitbody.wav"
+
+SWEP.ActivePos = Vector(0, -2, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.Animations = {
+    ["fire"] = {
+        Source = "shoot",
+        Time = 0.5,
+        MinProgress = 0.5,
+    },
+    ["fire_iron"] = {
+        Source = "idle",
+        Time = 0.5,
+        MinProgress = 0.5,
+    },
+    ["cycle"] = {
+        Source = "cycle",
+        ShellEjectAt = 0.05,
+        Time = 0.5,
+        TPAnim = ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN,
+    },
+    ["reload_empty"] = {
+        Source = "reload_empty",
+        ShellEjectAt = 1.85,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 1,
+        LHIKEaseOut = 0.35
+    },
+    ["reload_longmag_empty"] = {
+        Source = "reload_longmag_empty",
+        ShellEjectAt = 1.85,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 1,
+        LHIKEaseOut = 0.35
+    },
+}
+
+function SWEP:DoShootSound(sndoverride, dsndoverride, voloverride, pitchoverride)
+    local fsound = self.ShootSound
+    local suppressed = self:GetBuff_Override("Silencer")
+
+    if suppressed then
+        fsound = self.ShootSoundSilenced
+    end
+
+    local firstsound = self.FirstShootSound
+
+    if self:GetBurstCount() == 1 and firstsound then
+        fsound = firstsound
+
+        local firstsil = self.FirstShootSoundSilenced
+
+        if suppressed then
+            fsound = firstsil and firstsil or self.ShootSoundSilenced
+        end
+    end
+
+    local lastsound = self.LastShootSound
+
+    local clip = self:Clip1()
+
+    if clip == 1 and lastsound then
+        fsound = lastsound
+
+        local lastsil = self.LastShootSoundSilenced
+
+        if suppressed then
+            fsound = lastsil and lastsil or self.ShootSoundSilenced
+        end
+    end
+
+    fsound = self:GetBuff_Hook("Hook_GetShootSound", fsound)
+
+    local distancesound = self.DistantShootSound
+
+    if suppressed then
+        distancesound = self.DistantShootSoundSilenced
+    end
+
+    distancesound = self:GetBuff_Hook("Hook_GetDistantShootSound", distancesound)
+
+    local spv = self.ShootPitchVariation
+    local volume = self.ShootVol
+    local pitch  = self.ShootPitch * math.Rand(1 - spv, 1 + spv) * self:GetBuff_Mult("Mult_ShootPitch")
+
+    local v = ArcCW.ConVars["weakensounds"]:GetFloat()
+
+    volume = volume - v
+
+    volume = volume * self:GetBuff_Mult("Mult_ShootVol")
+
+    volume = math.Clamp(volume, 50, 140)
+    pitch  = math.Clamp(pitch, 0, 255)
+
+    if    sndoverride        then    fsound    = sndoverride end
+    if    dsndoverride    then    distancesound = dsndoverride end
+    if    voloverride        then    volume    = voloverride end
+    if    pitchoverride    then    pitch    = pitchoverride end
+
+    if distancesound then self:MyEmitSound(distancesound, 140, pitch, 0.25, CHAN_WEAPON) end
+
+    if fsound then self:MyEmitSound(fsound, volume, pitch, 1, CHAN_STATIC) end
+
+    local data = {
+        sound   = fsound,
+        volume  = volume,
+        pitch   = pitch,
+    }
+
+    self:GetBuff_Hook("Hook_AddShootSound", data)
+end
