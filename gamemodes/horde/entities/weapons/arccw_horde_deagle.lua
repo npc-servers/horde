de004e40bfc8458424ddd809f8e217d26c33ac42
func_brush <jacobamadolopez2006@gmail.com>
2025-08-29 22:37:57 -0700

    GSO Pistol Improvements

--- gamemodes/horde/entities/weapons/arccw_horde_deagle.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_deagle.lua
@@ -3,14 +3,55 @@ if CLIENT then
     SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_go_deagle")
     killicon.Add("arccw_horde_deagle", "arccw/weaponicons/arccw_go_deagle", Color(0, 0, 0, 255))
 end
+
 SWEP.Base = "arccw_go_deagle"
-SWEP.Spawnable = true -- this obviously has to be set to true
-SWEP.Category = "ArcCW - Horde" -- edit this if you like
+
+SWEP.Spawnable = true
+SWEP.Category = "ArcCW - Horde"
 SWEP.AdminOnly = false
 
 SWEP.PrintName = "Desert Eagle"
-SWEP.Slot = 1
+
 SWEP.ViewModel = "models/weapons/arccw_go/v_pist_deagle.mdl"
 SWEP.WorldModel = "models/weapons/arccw_go/v_pist_deagle.mdl"
 
-SWEP.Delay = 60 / 300 -- 60 / RPM.
+SWEP.Damage = 134
+SWEP.DamageMin = 85
+
+SWEP.RecoilPunch = 0
+
+SWEP.Delay = 60 / 150
+
+SWEP.ShootSound = "ArcCW_Horde.GSO.Deagle_Fire"
+SWEP.ShootSoundSilenced = "ArcCW_Horde.GSO.Deagle_Fire_Sil"
+SWEP.DistantShootSound = "ArcCW_Horde.GSO.Deagle_Fire_Dist"
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.RejectAttachments = {["go_fore_bipod"] = true, ["go_foregrip_angled"] = true}
+
+sound.Add( {
+    name = "ArcCW_Horde.GSO.Deagle_Fire",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 90,
+    pitch = 100,
+    sound = {")arccw_go/deagle/deagle_01.wav",")arccw_go/deagle/deagle_02.wav"}
+} )
+sound.Add( {
+    name = "ArcCW_Horde.GSO.Deagle_Fire_Sil",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 75,
+    pitch = 100,
+    sound = ")arccw_go/mosin_suppressed_fp.wav"
+} )
+sound.Add( {
+    name = "ArcCW_Horde.GSO.Deagle_Fire_Dist",
+    channel = CHAN_WEAPON,
+    volume = 0.25,
+    level = 140,
+    pitch = 100,
+    sound = "arccw_go/deagle/deagle-1-distant.wav"
+} )
