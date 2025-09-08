199471d3ecb07b1be10f91fdfe227fcddff5d987
SleepySophie <77075160+SleepySophie@users.noreply.github.com>
2025-09-08 02:37:03 +0200

    Fixed Hulk hp scaling

--- gamemodes/horde/entities/entities/npc_vj_horde_shadow_hulk/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_shadow_hulk/init.lua
@@ -6,7 +6,7 @@ include('shared.lua')
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
 ENT.Model = {"models/horde/hulk/hulk.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
-ENT.StartHealth = 100
+ENT.StartHealth = 200
 ENT.HullType = HULL_HUMAN
 ---------------------------------------------------------------------------------------------------------------------------------------------
 ENT.VJ_NPC_Class = {"CLASS_PLAYER_ALLY", "CLASS_COMBINE"} -- NPCs with the same class with be allied to each other
@@ -119,7 +119,8 @@ function ENT:CustomOnInitialize()
     self:SetRenderMode(RENDERMODE_TRANSCOLOR)
     self:SetColor(Color(0, 0, 50, 200))
 	self.MeleeAttackDamage = 2.75 * (self.MeleeAttackDamage + 6 * self.properties.level)
-	self:SetHealth(2 * (90 + 32 * self.properties.level))
+	self.StartHealth = math.floor(self.StartHealth + 85 * self.properties.level)
+	self:SetHealth(self.StartHealth)
 	self:AddRelationship("npc_manhack D_LI 99")
     --self:EmitSound("horde/lesion/lesion_roar.ogg", 1500, 80, 1, CHAN_STATIC)
 end
