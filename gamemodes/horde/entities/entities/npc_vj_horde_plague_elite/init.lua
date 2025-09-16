2c50262ecc8c3d0fdf1b7e1f61b92128ce8b8ec1
func_brush <jacobamadolopez2006@gmail.com>
2025-09-16 16:42:29 -0700

    Plague Soldier & Elite Improvements

--- gamemodes/horde/entities/entities/npc_vj_horde_plague_elite/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_plague_elite/init.lua
@@ -1,63 +1,27 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2015 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/
+
 ENT.Model = "models/combine_super_soldier.mdl" -- Leave empty if using more than one model
 ENT.StartHealth = 1050
-ENT.VJ_NPC_Class = {"CLASS_ZOMBIE", "CLASS_XEN"}
-ENT.MeleeAttackDamage = 30
-ENT.MoveType = MOVETYPE_STEP
-ENT.HullType = HULL_HUMAN
-ENT.SightDistance = 10000 -- How far it can see
-ENT.SightAngle = 80 -- The sight angle | Example: 180 would make the it see all around it | Measured in degrees and then converted to radians
-ENT.TurningSpeed = 20 -- How fast it can turn
-ENT.Bleeds = true -- Does the SNPC bleed? (Blood decal, particle, etc.)
-ENT.BloodColor = "Red" -- The blood type, this will determine what it should use (decal, particle, etc.)
-ENT.HasBloodParticle = true -- Does it spawn a particle when damaged?
-ENT.HasBloodDecal = true -- Does it spawn a decal when damaged?
-ENT.HasBloodPool = false -- Does it have a blood pool?
-ENT.Flinches = 1 -- 0 = No Flinch | 1 = Flinches at any damage | 2 = Flinches only from certain damages
-ENT.FlinchingChance = 12 -- chance of it flinching from 1 to x | 1 will make it always flinch
-ENT.FlinchingSchedules = {SCHED_FLINCH_PHYSICS} -- If self.FlinchUseACT is false the it uses this | Common: SCHED_BIG_FLINCH, SCHED_SMALL_FLINCH, SCHED_FLINCH_PHYSICS
-ENT.MoveWhenDamagedByEnemy = false -- Should the SNPC move when being damaged by an enemy?
-ENT.MoveWhenDamagedByEnemySCHED1 = SCHED_FORCED_GO_RUN -- The schedule it runs when MoveWhenDamagedByEnemy code is ran | The first # in math.random
-ENT.MoveWhenDamagedByEnemySCHED2 = SCHED_FORCED_GO_RUN -- The schedule it runs when MoveWhenDamagedByEnemy code is ran | The second # in math.random
-ENT.NextMoveWhenDamagedByEnemy1 = 3 -- Next time it moves when getting damaged | The first # in math.random
-ENT.NextMoveWhenDamagedByEnemy2 = 3.5 -- Next time it moves when getting damaged | The second # in math.random
-ENT.HasAllies = true -- Put to false if you want it not to have any allies
-ENT.HasMeleeAttack = true -- Should the SNPC have a melee attack?
-ENT.HasGrenadeAttack = true -- Should the SNPC have a grenade attack?
-ENT.NextThrowGrenadeTime1 = 10 -- Time until it runs the throw grenade code again | The first # in math.random
-ENT.NextThrowGrenadeTime2 = 15 -- Time until it runs the throw grenade code again | The second # in math.random
-ENT.ThrowGrenadeChance = 1 -- Chance that it will throw the grenade | Set to 1 to throw all the time
-ENT.GrenadeAttackThrowDistance = 1000 -- How far it can throw grenades
-ENT.GrenadeAttackThrowDistanceClose = 500 -- How close until it stops throwing grenades
-ENT.AnimTbl_GrenadeAttack = {"grenThrow"} -- Grenade Attack Animations
-ENT.GrenadeAttackAnimationDelay = 0 -- It will wait certain amount of time before playing the animation
-ENT.GrenadeAttackAnimationStopAttacks = true -- Should it stop attacks for a certain amount of time?
-ENT.GrenadeAttackEntity = "npc_grenade_frag" -- The entity that the SNPC throws | Half Life 2 Grenade: "npc_grenade_frag"
-ENT.FootStepTimeRun = 0.3 -- Next foot step sound when it is running
-ENT.FootStepTimeWalk = 0.5 -- Next foot step sound when it is walking
-ENT.CallForBackUpOnDamage = false -- Should the SNPC call for help when damaged? (Only happens if the SNPC hasn't seen a enemy)
-ENT.CanDetectDangers = false
-ENT.MoveOrHideOnDamageByEnemy = false
-ENT.WeaponSpread = 1.5
-ENT.Weapon_FiringDistanceFar = 1750
-ENT.WeaponReload_FindCover = false
-ENT.Horde_Plague_Soldier = true
-ENT.WeaponAttackSecondaryTimeUntilFire = 2
+ENT.HullType = MEDIUM_TALL_HULL
 ENT.EntitiesToNoCollide = {"npc_vj_horde_zombine", "npc_vj_horde_plague_soldier"}
-ENT.CallForHelp = false
-ENT.CanInvestigate = false
+ENT.VJ_NPC_Class = {"CLASS_ZOMBIE", "CLASS_XEN"}
+ENT.AnimTbl_Run = {ACT_WALK}
+ENT.ConstantlyFaceEnemy_IfVisible = false
+ENT.ConstantlyFaceEnemy_IfAttacking = true
+ENT.BloodColor = "Red"
+ENT.MoveOrHideOnDamageByEnemy = false
 ENT.DropWeaponOnDeath = false
-
-ENT.SoundTbl_Pain = {
-"npc/combine_soldier/pain1.wav",
-"npc/combine_soldier/pain2.wav",
-"npc/combine_soldier/pain3.wav"}
+ENT.HasItemDropsOnDeath = false
+ENT.WeaponSpread = 1
+ENT.Weapon_FiringDistanceFar = 1200
+ENT.HasWeaponBackAway = false
+ENT.CanCrouchOnWeaponAttack = false
+ENT.HasShootWhileMoving = false
+ENT.WeaponReload_FindCover = false
+ENT.MoveRandomlyWhenShooting = false
+ENT.WaitForEnemyToComeOut = false
+ENT.Horde_Plague_Soldier = true
 
 ENT.SoundTbl_FootStep = {
 	"npc/combine_soldier/gear1.wav",
@@ -65,41 +29,67 @@ ENT.SoundTbl_FootStep = {
 	"npc/combine_soldier/gear3.wav",
 	"npc/combine_soldier/gear4.wav",
 	"npc/combine_soldier/gear5.wav",
-	"npc/combine_soldier/gear6.wav"}
-ENT.SoundTbl_Idle = {"zsszombine/idle1.wav","zsszombine/idle2.wav","zsszombine/idle3.wav","zsszombine/idle4.wav","zsszombine/idle5.wav"}
-ENT.SoundTbl_Alert = {"zsszombine/alert1.wav","zsszombine/alert2.wav","zsszombine/alert3.wav","zsszombine/alert4.wav","zsszombine/alert5.wav","zsszombine/alert6.wav"}
+	"npc/combine_soldier/gear6.wav"
+}
+ENT.SoundTbl_Idle = {
+	"npc/combine_soldier/assault1.wav",
+	"npc/combine_soldier/assault2.wav",
+	"npc/combine_soldier/assault3.wav"
+}
+ENT.SoundTbl_Alert = {
+	"npc/combine_soldier/go_alert1.wav",
+	"npc/combine_soldier/go_alert2.wav",
+	"npc/combine_soldier/go_alert3.wav"
+}
+ENT.SoundTbl_Suppressing = {
+	"npc/combine_soldier/announce1.wav",
+	"npc/combine_soldier/announce2.wav",
+	"npc/combine_soldier/announce3.wav"
+}
+ENT.SoundTbl_Pain = {
+	"npc/combine_soldier/pain1.wav",
+	"npc/combine_soldier/pain2.wav",
+	"npc/combine_soldier/pain3.wav"
+}
+ENT.SoundTbl_Death = {
+	"npc/combine_soldier/die1.wav",
+	"npc/combine_soldier/die2.wav",
+	"npc/combine_soldier/die3.wav"
+}
 
-	-- ====== Sound Pitch ====== --
--- Higher number = Higher pitch | Lower number = Lower pitch
--- Highest number is 254
-	-- !!! Important variables !!! --
 ENT.UseTheSameGeneralSoundPitch = true
-	-- If set to true, it will make the game decide a number when the SNPC is created and use it for all sound pitches set to "UseGeneralPitch"
-	-- It picks the number between the two variables below:
+
 ENT.GeneralSoundPitch1 = 75
 ENT.GeneralSoundPitch2 = 75
 
+ENT.NextSummonTime = CurTime()
+ENT.NextSummonCooldown = 15
+
+local sdCombine_Radio_On = "npc/combine_soldier/clik.wav"
+local sdCombine_Radio_Off = "npc/combine_soldier/click_off.wav"
+local sdPlague_Summon = "npc/combine_soldier/go!.wav"
+local sdPlague_Die = "npc/combine_soldier/click_terminated.wav"
+
 function ENT:CustomOnInitialize()
 	self:SetModelScale(1.25)
-	self:AddRelationship("npc_headcrab_poison D_LI 99")
-	self:AddRelationship("npc_headcrab_fast D_LI 99")
-	self:SetColor(Color(50,50,50))
+	self:SetColor(Color(50, 50, 50))
 
-	local p = math.random()
 	self:Give("weapon_vj_horde_ar2")
+
 	local pos = Vector()
 	local ang = Angle()
 	local attach_id = self:LookupAttachment("eyes")
 	local attach = self:GetAttachment(attach_id)
 	pos = attach.Pos
 	ang = attach.Ang
-	pos.x = pos.x - 3
+	pos.x = pos.x - 6
 	pos.z = pos.z - 6
 	pos.y = pos.y
+
 	self.model = ents.Create("prop_dynamic")
 	self.model:SetModel("models/headcrabblack.mdl")
+	self.model:SetCollisionGroup(0)
 	self.model:SetColor(Color(255, 0, 0))
-	--self.model = ClientsideModel("models/headcrabblack.mdl", RENDERGROUP_OPAQUE)
 	self.model:SetSequence("ragdoll")
 	self.model:SetPos(pos)
 	self.model:SetAngles(ang)
@@ -107,43 +97,65 @@ function ENT:CustomOnInitialize()
 	self.model:SetParent(self, attach_id)
 	self.model:SetModelScale(1.5)
 
-	self:EmitSound("npc/combine_gunship/see_enemy.wav", 3000, 100, 2, CHAN_STATIC)
+	VJ_EmitSound(self, "npc/combine_gunship/see_enemy.wav", 140, 100)
 end
 
 local defAng = Angle(0, 0, 0)
 
-ENT.ZBoss_NextMiniBossSpawnT = 0
 function ENT:CustomOnThink_AIEnabled()
-	if IsValid(self:GetEnemy()) && CurTime() > self.ZBoss_NextMiniBossSpawnT && (!IsValid(self.MiniBoss1) || !IsValid(self.MiniBoss2)) then
-		self:VJ_ACT_PLAYACTIVITY("vjseq_releasecrab", true, false, false)
-		ParticleEffect("aurora_shockwave_debris", self:GetPos(), defAng, nil)
-		ParticleEffect("aurora_shockwave", self:GetPos(), defAng, nil)
-		self:EmitSound("horde/plague_elite/summon.ogg")
+	local enemyDistance = self.NearestPointToEnemyDistance
 
-		if (!IsValid(self.MiniBoss1)) then
-			self.MiniBoss1 = ents.Create("npc_vj_horde_plague_soldier")
-			self.MiniBoss1:SetPos(self:GetPos() + self:GetRight()*45)
-			self.MiniBoss1:SetAngles(self:GetAngles())
-			self.MiniBoss1:Spawn()
-			self.MiniBoss1:SetOwner(self)
+	if IsValid( self:GetEnemy() ) and CurTime() > self.NextSummonTime and enemyDistance <= 1000 then
+		if not IsValid(self.MiniBoss1) or not IsValid(self.MiniBoss2) then
+			self:PlaySoundSystem("Alert", sdPlague_Summon)
+			self:VJ_ACT_PLAYACTIVITY("vjseq_signal_advance", true, false, false)
+
+			ParticleEffect("aurora_shockwave_debris", self:GetPos(), defAng, nil)
+			ParticleEffect("aurora_shockwave", self:GetPos(), defAng, nil)
+
+			VJ_EmitSound(self, "horde/plague_elite/summon.ogg", 140, 100)
+
+			if not IsValid(self.MiniBoss1) then
+				self.MiniBoss1 = ents.Create("npc_vj_horde_plague_soldier")
+				self.MiniBoss1:SetPos( self:GetPos() + self:GetRight() * 50 + self:GetUp() * 5 )
+				self.MiniBoss1:Spawn()
+				self.MiniBoss1:SetOwner( self )
+				self.MiniBoss1:Activate()
+			end
+
+			if not IsValid(self.MiniBoss2) then
+				self.MiniBoss2 = ents.Create("npc_vj_horde_zombine")
+				self.MiniBoss2:SetPos( self:GetPos() + self:GetRight() * -50 + self:GetUp() * 5 )
+				self.MiniBoss2:Spawn()
+				self.MiniBoss2:SetOwner( self )
+				self.MiniBoss2:Activate()
+			end
 		end
-
-		if (!IsValid(self.MiniBoss2)) then
-			self.MiniBoss2 = ents.Create("npc_vj_horde_zombine")
-			self.MiniBoss2:SetPos(self:GetPos() + self:GetRight()*-45)
-			self.MiniBoss2:SetAngles(self:GetAngles())
-			self.MiniBoss2:Spawn()
-			self.MiniBoss2:SetOwner(self)
-		end
-
-		self.ZBoss_NextMiniBossSpawnT = CurTime() + 15
+		self.NextSummonTime = CurTime() + self.NextSummonCooldown
 	end
 end
 
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2015 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/
+function ENT:OnPlayCreateSound(sdData, sdFile)
+	if VJ_HasValue(self.SoundTbl_MeleeAttack, sdFile) or VJ_HasValue(self.SoundTbl_IdleDialogue, sdFile) or VJ_HasValue(self.SoundTbl_Pain, sdFile) or VJ_HasValue(self.SoundTbl_Death, sdFile) then 
+		return 
+	end
+	VJ_EmitSound(self, sdCombine_Radio_On, 75, 75)
+	timer.Simple(SoundDuration(sdFile) * 1.25, function() 
+		if IsValid(self) && sdData:IsPlaying() then 
+			VJ_EmitSound(self, sdCombine_Radio_Off, 75, 75) 
+		end 
+	end)
+end
 
-VJ.AddNPC("Hulk","npc_vj_horde_plague_elite", "Zombies")
+function ENT:CustomOnKilled(dmginfo, hitgroup)
+	if IsValid(self.MiniBoss1) then
+		self.MiniBoss1:Remove() 
+	end
+
+	if IsValid(self.MiniBoss2) then
+    	self.MiniBoss2:Remove() 
+    end
+    VJ_EmitSound(self, sdPlague_Die, 80, 100)
+end
+
+VJ.AddNPC("Plague Elite","npc_vj_horde_plague_elite", "Zombies")
--- gamemodes/horde/entities/entities/npc_vj_horde_plague_soldier/shared.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_plague_soldier/shared.lua
@@ -8,10 +8,10 @@ ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Zombies"
 
 if (CLIENT) then
-local Name = "Plague Soldier"
-local LangName = "npc_vj_horde_plague_soldier"
-language.Add(LangName, Name)
-killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
-language.Add("#"..LangName, Name)
-killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
+	local Name = "Plague Soldier"
+	local LangName = "npc_vj_horde_plague_soldier"
+	language.Add(LangName, Name)
+	killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
+	language.Add("#"..LangName, Name)
+	killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
 end
