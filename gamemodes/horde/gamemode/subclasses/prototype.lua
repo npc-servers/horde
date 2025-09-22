1381b85fe9c63e8c81dce6d93ef858763dc8b62f
func_brush <jacobamadolopez2006@gmail.com>
2025-09-22 07:16:16 -0700

    styling fixes

--- gamemodes/horde/gamemode/subclasses/prototype.lua
+++ gamemodes/horde/gamemode/subclasses/prototype.lua
@@ -8,8 +8,8 @@ Close-range fighter with unique abilities.]]
 
 SUBCLASS.BasePerk = "prototype_base"
 SUBCLASS.Perks = {
-    [ 1 ] = { title = "Hemorrhage", choices = { "prototype_gluttonous_maw", "prototype_hemo_siphon" } },
-    [ 2 ] = { title = "Momentum", choices = { "prototype_overdrive", "prototype_countermeasures" } },
-    [ 3 ] = { title = "Defiance", choices = { "prototype_knuckleblaster", "prototype_feedbacker" } },
-    [ 4 ] = { title = "Pestilence", choices = { "prototype_rupture", "prototype_ascendent" } },
+    [1] = { title = "Hemorrhage", choices = { "prototype_gluttonous_maw", "prototype_hemo_siphon" } },
+    [2] = { title = "Momentum", choices = { "prototype_overdrive", "prototype_countermeasures" } },
+    [3] = { title = "Defiance", choices = { "prototype_knuckleblaster", "prototype_feedbacker" } },
+    [4] = { title = "Pestilence", choices = { "prototype_rupture", "prototype_ascendent" } },
 }
