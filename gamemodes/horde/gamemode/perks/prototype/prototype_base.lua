ca2a2d0b00db7d8c3d007f4accb139a7a8b3d0f3
func_brush <jacobamadolopez2006@gmail.com>
2025-09-22 07:03:37 -0700

    styling fixes

--- gamemodes/horde/gamemode/perks/prototype/prototype_base.lua
+++ gamemodes/horde/gamemode/perks/prototype/prototype_base.lua
@@ -7,70 +7,67 @@ Complexity: MEDIUM
 
 Leech 1 health at close range.
 
-Has access to light weapons and shotguns.]]
+Has access to light weapons and shotguns. ]]
 
 PERK.Params = {
-    [1] = {percent = true, level = 0.01, base = 0.25, max = 0.50, classname = "Prototype"},
-    [2] = {value = 0.25, percent = true},
-    [3] = {value = 0.01, percent = true},
-    [4] = {value = 0.50, percent = true},
+    [1] = { percent = true, level = 0.01, base = 0.25, max = 0.50, classname = "Prototype" },
+    [2] = { value = 0.25, percent = true },
+    [3] = { value = 0.01, percent = true },
+    [4] = { value = 0.50, percent = true },
 }
 
 PERK.Hooks = {}
-PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
+PERK.Hooks.Horde_OnSetPerk = function( ply, perk )
     if SERVER and perk == "prototype_base" then
     end
 end
 
-PERK.Hooks.Horde_OnUnsetPerk = function(ply, perk)
+PERK.Hooks.Horde_OnUnsetPerk = function( ply, perk )
     if SERVER and perk == "prototype_base" then
     end
 end
 
-PERK.Hooks.Horde_PrecomputePerkLevelBonus = function (ply)
+PERK.Hooks.Horde_PrecomputePerkLevelBonus = function( ply )
     if SERVER then
-        ply:Horde_SetPerkLevelBonus("prototype_base", math.min(0.50, 0.25 + 0.01 * ply:Horde_GetLevel("Prototype")))
+        ply:Horde_SetPerkLevelBonus( "prototype_base", math.min( 0.50, 0.25 + 0.01 * ply:Horde_GetLevel( "Prototype" ) ) )
     end
 end
 
-PERK.Hooks.Horde_OnSetMaxHealth = function (ply)
+PERK.Hooks.Horde_OnSetMaxHealth = function( ply )
     if SERVER then
         ply.Horde_PrototypeGetMaxHealth = ply:GetMaxHealth()
     end
 end
 
-PERK.Hooks.Horde_OnPlayerDamage = function (ply, npc, bonus, hitgroup, dmginfo)
-    if not ply:Horde_GetPerk("prototype_base") then return end
-    if not HORDE:IsBallisticDamage(dmginfo) then return end
+PERK.Hooks.Horde_OnPlayerDamage = function( ply, npc, bonus, hitgroup, dmginfo )
+    if not ply:Horde_GetPerk( "prototype_base" ) then return end
+    if not HORDE:IsBallisticDamage( dmginfo ) then return end
 
     local hitPos = dmginfo:GetDamagePosition()
     local selfPos = ply:GetPos()
-    local sqrDist = hitPos:DistToSqr(selfPos)
+    local sqrDist = hitPos:DistToSqr( selfPos )
 
-    local plyLevel = ply:Horde_GetPerkLevelBonus("prototype_base")
+    local plyLevel = ply:Horde_GetPerkLevelBonus( "prototype_base" )
     if sqrDist < 15000 then
         bonus.increase = bonus.increase + plyLevel
 
         if ply:Health() == ply.Horde_PrototypeGetMaxHealth then return end
         if dmginfo:GetDamage() <= 0 then return end
 
-        ply:EmitSound( "horde/player/prototype/HpGet.wav", 75, math.random(95, 105), 1, CHAN_AUTO)
-        HORDE:SelfHeal(ply, ply:Horde_GetPerk("prototype_gluttonous_maw") and 5 or 1)
+        ply:EmitSound( "horde/player/prototype/HpGet.wav", 75, math.random( 95, 105 ), 1, CHAN_AUTO )
+        HORDE:SelfHeal( ply, ply:Horde_GetPerk( "prototype_gluttonous_maw" ) and 5 or 1 )
     elseif sqrDist < 30000 then
         bonus.increase = bonus.increase + plyLevel / 2
 
         if ply:Health() == ply.Horde_PrototypeGetMaxHealth then return end
-        if not ply:Horde_GetPerk("prototype_hemo_siphon") then return end
+        if not ply:Horde_GetPerk( "prototype_hemo_siphon" ) then return end
         if dmginfo:GetDamage() <= 0 then return end
 
-        ply:EmitSound( "horde/player/prototype/HpGet.wav", 75, math.random(95, 105), 1, CHAN_AUTO)
-        HORDE:SelfHeal(ply, 1)
+        ply:EmitSound( "horde/player/prototype/HpGet.wav", 75, math.random( 95, 105 ), 1, CHAN_AUTO )
+        HORDE:SelfHeal( ply, 1 )
     elseif sqrDist < 45000 then
         bonus.increase = bonus.increase + plyLevel / 4
     elseif sqrDist < 60000 then
         return
     end
 end
-
-
--- Distances: 15000 full bonus + leeching, 30000 1/2 bonus, 45000 1/3 bonus, 60000 no bonus.
