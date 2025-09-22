9d36ac9ba9a49e4ba92983e65b8e27aad18c9ebb
func_brush <jacobamadolopez2006@gmail.com>
2025-09-22 07:09:10 -0700

    styling fixes

--- gamemodes/horde/gamemode/perks/prototype/prototype_countermeasures.lua
+++ gamemodes/horde/gamemode/perks/prototype/prototype_countermeasures.lua
@@ -4,17 +4,19 @@ PERK.Description = "Immune to Poison, Bleed, and Necrosis."
 
 PERK.Hooks = {}
 
-PERK.Hooks.Horde_OnPlayerDebuffApply = function (ply, debuff, bonus, inflictor)
-    if not ply:Horde_GetPerk("prototype_countermeasures")  then return end
+PERK.Hooks.Horde_OnPlayerDebuffApply = function( ply, debuff, bonus, inflictor )
+    if not ply:Horde_GetPerk( "prototype_countermeasures" ) then return end
+    
     if debuff == HORDE.Status_Bleeding or debuff == HORDE.Status_Necrosis then
         bonus.apply = 0
         return true
     end
 end
 
-PERK.Hooks.Horde_OnPlayerDamageTaken = function (ply, dmginfo, bonus)
-    if not ply:Horde_GetPerk("prototype_countermeasures") then return end
-    if HORDE:IsPoisonDamage(dmginfo) then
+PERK.Hooks.Horde_OnPlayerDamageTaken = function( ply, dmginfo, bonus )
+    if not ply:Horde_GetPerk( "prototype_countermeasures" ) then return end
+    
+    if HORDE:IsPoisonDamage( dmginfo ) then
         bonus.resistance = bonus.resistance + 1.0
     end
 end
