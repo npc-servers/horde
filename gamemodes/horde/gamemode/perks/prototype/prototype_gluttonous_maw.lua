2e2c7df9a19632033eb66ca4ace2d36199ec789d
func_brush <jacobamadolopez2006@gmail.com>
2025-09-22 17:05:51 -0700

    leech buff

--- gamemodes/horde/gamemode/perks/prototype/prototype_gluttonous_maw.lua
+++ gamemodes/horde/gamemode/perks/prototype/prototype_gluttonous_maw.lua
@@ -1,5 +1,5 @@
 PERK.PrintName = "Gluttonous Maw"
 PERK.Icon = "materials/perks/charge.png"
-PERK.Description = "Increases Leech amount to 5."
+PERK.Description = "Increases Leech amount to 10."
 
 PERK.Hooks = {}
