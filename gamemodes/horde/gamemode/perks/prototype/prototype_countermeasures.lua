19b712c9e9c3abe656b1a37662ae72d4951383b9
func_brush <jacobamadolopez2006@gmail.com>
2025-09-22 21:29:16 -0700

    balance changes

--- gamemodes/horde/gamemode/perks/prototype/prototype_countermeasures.lua
+++ gamemodes/horde/gamemode/perks/prototype/prototype_countermeasures.lua
@@ -1,14 +1,14 @@
 PERK.PrintName = "Countermeasures"
 PERK.Icon = "materials/perks/breathing_technique.png"
-PERK.Description = "25% resistance to Poison, Bleed, and Necrosis."
+PERK.Description = "30% resistance to Poison. 15% less Bleed and Decay buildup."
 
 PERK.Hooks = {}
 
 PERK.Hooks.Horde_OnPlayerDebuffApply = function( ply, debuff, bonus, inflictor )
     if not ply:Horde_GetPerk( "prototype_countermeasures" ) then return end
     
-    if debuff == HORDE.Status_Bleeding or debuff == HORDE.Status_Necrosis then
-        bonus.apply = 0.25
+    if debuff == HORDE.Status_Bleeding or debuff == HORDE.Status_Decay then
+        bonus.less = bonus.less * 0.85
         return true
     end
 end
@@ -17,6 +17,6 @@ PERK.Hooks.Horde_OnPlayerDamageTaken = function( ply, dmginfo, bonus )
     if not ply:Horde_GetPerk( "prototype_countermeasures" ) then return end
     
     if HORDE:IsPoisonDamage( dmginfo ) then
-        bonus.resistance = bonus.resistance + 0.75
+        bonus.resistance = bonus.resistance + 0.30
     end
 end
