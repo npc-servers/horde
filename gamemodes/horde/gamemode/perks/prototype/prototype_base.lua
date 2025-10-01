f8b091a0b8c83a99bce5fe442c02b0849320288b
func_brush <jacobamadolopez2006@gmail.com>
2025-10-01 14:45:07 -0700

    Prototype Survivor Subclass (#351)
    * Prototype Survivor Subclass

---
+++ gamemodes/horde/gamemode/perks/prototype/prototype_base.lua
@@ -0,0 +1,74 @@
+PERK.PrintName = "Prototype Base"
+PERK.Description = [[
+The Prototype class is a close-range fighter with unique abilities.
+Complexity: MEDIUM
+
+{1} increased Physical damage at close-range ({2} + {3} per level, up to {4}).
+
+Leech health when near enemies.
+
+Has access to light weapons and shotguns.]]
+
+PERK.Params = {
+    [1] = { percent = true, level = 0.01, base = 0.05, max = 0.30, classname = "Prototype" },
+    [2] = { value = 0.05, percent = true },
+    [3] = { value = 0.01, percent = true },
+    [4] = { value = 0.30, percent = true },
+}
+
+PERK.Hooks = {}
+
+if not SERVER then return end
+
+PERK.Hooks.Horde_OnUnsetPerk = function( ply, perk )
+    if perk == "prototype_base" then
+        ply.Horde_PrototypeGetMaxHealth = nil
+    end
+end
+
+PERK.Hooks.Horde_PrecomputePerkLevelBonus = function( ply )
+    ply:Horde_SetPerkLevelBonus( "prototype_base", math.min( 0.30, 0.05 + 0.01 * ply:Horde_GetLevel( "Prototype" ) ) )
+end
+
+PERK.Hooks.Horde_OnSetMaxHealth = function( ply )
+    if not ply:Horde_GetPerk( "prototype_base" ) then return end
+    
+    timer.Simple( 0, function() 
+        if IsValid( ply ) then 
+            ply.Horde_PrototypeGetMaxHealth = ply:GetMaxHealth() 
+        end 
+    end )
+end
+
+PERK.Hooks.Horde_OnPlayerDamage = function( ply, npc, bonus, hitgroup, dmginfo )
+    if not ply:Horde_GetPerk( "prototype_base" ) then return end
+    if not HORDE:IsPhysicalDamage( dmginfo ) then return end
+
+    local plyLevel = ply:Horde_GetPerkLevelBonus( "prototype_base" )
+
+    local hitPos = dmginfo:GetDamagePosition()
+    local selfPos = ply:GetPos()
+    local sqrDist = hitPos:DistToSqr( selfPos )
+
+    if sqrDist < 15000 then
+        bonus.increase = bonus.increase + plyLevel
+    elseif sqrDist < 30000 then
+        bonus.increase = bonus.increase + plyLevel / 2
+    elseif sqrDist < 45000 then
+        bonus.increase = bonus.increase + plyLevel / 4
+    elseif sqrDist < 60000 then
+        return
+    end
+
+    if ply:Health() == ply.Horde_PrototypeGetMaxHealth then return end
+    if dmginfo:GetDamage() <= 0 then return end
+
+    if sqrDist < 30000 then
+        HORDE:SelfHeal( ply, ply:Horde_GetPerk( "prototype_gluttonous_maw" ) and 3 or 1 )
+        ply:EmitSound( ")horde/player/prototype/HpGet.wav", 75, math.random( 95, 105 ), 1, CHAN_AUTO )
+    elseif sqrDist < 60000 then
+        if not ply:Horde_GetPerk( "prototype_hemo_siphon" ) then return end
+        HORDE:SelfHeal( ply, 1 )
+        ply:EmitSound( ")horde/player/prototype/HpGet.wav", 75, math.random( 110, 120 ), 1, CHAN_AUTO )
+    end
+end
---
+++ gamemodes/horde/gamemode/perks/prototype/prototype_gluttonous_maw.lua
@@ -0,0 +1,5 @@
+PERK.PrintName = "Gluttonous Maw"
+PERK.Icon = "materials/perks/charge.png"
+PERK.Description = "Increases Leech amount to 3."
+
+PERK.Hooks = {}
