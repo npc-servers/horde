05b7139890a35601b024ed8df61b6eed454985f4
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-10-02 17:46:30 -0700

    Fix overlord aura ring

--- gamemodes/horde/entities/entities/horde_overlord_presence/cl_init.lua
+++ gamemodes/horde/entities/entities/horde_overlord_presence/cl_init.lua
@@ -1,22 +1,39 @@
-include("shared.lua")
+include( "shared.lua" )
+
+local opacityConvar = CreateClientConVar( "horde_overlord_aura_ring_opacity",
+    "100",
+    true,
+    false,
+    "Controls the Opacity of the Warden Aura Rings, the higher, the more opaque/visible, Set to 0 to disable Warden Aura rings from rendering",
+    0,
+    255
+)
+
+local opacity = opacityConvar:GetInt()
+local col = Color( 32, 0, 56, opacity )
+
+cvars.AddChangeCallback( "horde_overlord_aura_ring_opacity", function( _, _, newValue )
+    opacity = tonumber( newValue )
+    col.a = opacity
+end )
 
 local auras = auras or {}
 
 function ENT:Initialize()
-	-- Display only one sphere for local client. Sphere renders are extremely dank and costly
-    if self:GetParent() == LocalPlayer() then
-        auras[self] = true
-    end
+    auras[self] = true
 end
 
 function ENT:OnRemove()
-	auras[self] = nil
+    auras[self] = nil
 end
 
-hook.Add( "PreDraw_ImmersionBreakingCircles", "Draw_PresenceCircle", function()
-	if next(auras) == nil then return end
-	for aura in pairs(auras) do
-        --if not aura:GetParent() == LocalPlayer() then continue end
-        HORDE.MaskedSphereRing(aura:GetPos(), aura:GetCircleRadius() + 20, 16, 4, Color(32, 0, 56, 100))
+hook.Add( "PreDrawRings", "DrawWardenRings", function()
+    if next( auras ) == nil then return end
+
+    local set = {}
+    for aura in pairs( auras ) do
+        table.insert( set, { aura, aura:GetCircleRadius() or 160, 5, 30 } )
     end
-end)
+
+    rings.AddSet( set, col )
+end )
