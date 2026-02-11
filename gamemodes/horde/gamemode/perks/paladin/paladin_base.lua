44310d795afde63764e4ee6028acd11f2477787b
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-02-11 15:05:55 -0800

    move Horde_PostOnPlayerHeal to paladin base

--- gamemodes/horde/gamemode/perks/paladin/paladin_base.lua
+++ gamemodes/horde/gamemode/perks/paladin/paladin_base.lua
@@ -126,3 +126,18 @@ PERK.Hooks.PlayerButtonUp = function( ply, button )
 
     ply.Horde_PaladinShielding = nil
 end
+
+-- Armor on heal
+PERK.Hooks.Horde_PostOnPlayerHeal = function( ply, healinfo )
+    local healer = healinfo.healer
+    if not IsValid( healer ) then return end
+    if not healer:Horde_GetPerk( "paladin_base" ) then return end
+
+    -- Check if inside aura and if aura is owned by healer
+    local paladinAuraProvider = ply.Horde_PaladinAuraProvider
+    if not IsValid( paladinAuraProvider ) or paladinAuraProvider ~= healer then return end
+
+    -- Give 50% of healing as armor
+    if ply:Armor() == ply:GetMaxArmor() then return end
+    ply:SetArmor( math.Min( ply:GetMaxArmor(), ply:Armor() + healinfo.amount / 2 ) )
+end
--- gamemodes/horde/gamemode/status/buff/sv_paladin_aura.lua
+++ gamemodes/horde/gamemode/status/buff/sv_paladin_aura.lua
@@ -64,18 +64,3 @@ end )
 hook.Add( "DoPlayerDeath", "Horde_PaladinAura", function( victim )
     victim:Horde_RemovePaladinAura()
 end )
-
--- Armor on heal
-hook.Add( "Horde_PostOnPlayerHeal", "Horde_PaladinAura", function( ply, healinfo )
-    local healer = healinfo.healer
-    if not IsValid( healer ) then return end
-    if not healer:Horde_GetPerk( "paladin_base" ) then return end
-
-    -- Check if inside aura and if aura is owned by healer
-    local paladinAuraProvider = ply.Horde_PaladinAuraProvider
-    if not IsValid( paladinAuraProvider ) or paladinAuraProvider ~= healer then return end
-
-    -- Give 50% of healing as armor
-    if ply:Armor() == ply:GetMaxArmor() then return end
-    ply:SetArmor( math.Min( ply:GetMaxArmor(), ply:Armor() + healinfo.amount / 2 ) )
-end )
