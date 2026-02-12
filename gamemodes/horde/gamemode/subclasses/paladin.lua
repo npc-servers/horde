f08092b8f946f4b0ba71ef54fd81f1543787c7d6
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-02-12 10:28:27 -0800

    placeholder text

--- gamemodes/horde/gamemode/subclasses/paladin.lua
+++ gamemodes/horde/gamemode/subclasses/paladin.lua
@@ -8,8 +8,8 @@ Paladin is a tanky melee shieldbearer, using his shield and aura to protect and 
 banishing his enemies.]]
 SUBCLASS.BasePerk = "paladin_base"
 SUBCLASS.Perks = {
-    [1] = { title = "Inner Strength", choices = { "paladin_shield_bash", "paladin_smite" } },
-    [2] = { title = "Recuperate", choices = { "paladin_protectors_oath", "paladin_inquisitors_oath" } },
-    [3] = { title = "Codex", choices = { "paladin_providence", "paladin_dawnbrinder" } },
-    [4] = { title = "Ascension", choices = { "paladin_sanctuary", "paladin_rallying_presence" } },
+    [1] = { title = "Placeholder 1", choices = { "paladin_shield_bash", "paladin_smite" } },
+    [2] = { title = "Placeholder 2", choices = { "paladin_protectors_oath", "paladin_inquisitors_oath" } },
+    [3] = { title = "Placeholder 3", choices = { "paladin_providence", "paladin_dawnbrinder" } },
+    [4] = { title = "Placeholder 4", choices = { "paladin_sanctuary", "paladin_rallying_presence" } },
 }
