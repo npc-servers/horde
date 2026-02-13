8db64d481095dc07e523b719de6d594e4c6f08cd
func_brush <jacobamadolopez2006@gmail.com>
2026-02-12 23:11:01 -0800

    Zombie optimizations Pt.1 (#385)
    * optimizations/balance
    
    * optimizations
    
    * actwalk/actrun no table
    
    * optimizations + style fixes
    
    * style fixes

--- gamemodes/horde/entities/entities/npc_vj_horde_crawler/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_crawler/init.lua
@@ -1,35 +1,86 @@
-AddCSLuaFile("shared.lua")
-include('shared.lua')
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
+--[[---------------------------------------------
+	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------]]
+ENT.Model = "models/zombie/zm_fast.mdl"
+ENT.StartHealth = 50
 
-ENT.Model = {"models/zombie/zm_fast.mdl"}
-ENT.StartHealth = 30
-ENT.VJ_NPC_Class = {"CLASS_ZOMBIE","CLASS_XEN"}
+ENT.VJ_NPC_Class = { "CLASS_ZOMBIE", "CLASS_XEN" }
+
 ENT.BloodColor = "Red"
-ENT.MeleeAttackDamage = 5
-ENT.AnimTbl_MeleeAttack = {"vjseq_melee"}
-ENT.MeleeAttackDistance = 40
-ENT.MeleeAttackDamageDistance = 60
-ENT.TimeUntilMeleeAttackDamage = 0.25
-ENT.NextMeleeAttackTime = 1
-ENT.MeleeAttackExtraTimers = {0.5}
-ENT.FootStepTimeRun = 0.25
-ENT.DisableFootStepOnWalk = true
-ENT.AlertSounds_OnlyOnce = true
-ENT.HasExtraMeleeAttackSounds = true
-ENT.HasImpactSounds = false
 
-ENT.SoundTbl_FootStep = {"npc/fast_zombie/foot1.wav","npc/fast_zombie/foot2.wav","npc/fast_zombie/foot3.wav","npc/fast_zombie/foot4.wav"}
-ENT.SoundTbl_Idle = {"vfastzombie/fzombie_idle1.wav","vfastzombie/fzombie_idle2.wav","vfastzombie/fzombie_idle3.wav",")vfastzombie/fzombie_idle4.wav",")vfastzombie/fzombie_idle5.wav"}
-ENT.SoundTbl_Alert = {"vfastzombie/fzombie_alert1.wav","vfastzombie/fzombie_alert2.wav","vfastzombie/fzombie_alert3.wav"}
-ENT.SoundTbl_BeforeMeleeAttack = {"vfastzombie/attack1.wav","vfastzombie/attack2.wav","vfastzombie/attack3.wav"}
-ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
-ENT.SoundTbl_Pain = {"vfastzombie/fzombie_pain1.wav","vfastzombie/fzombie_pain2.wav","vfastzombie/fzombie_pain3.wav"}
-ENT.SoundTbl_Death = {"vfastzombie/fzombie_die1.wav","vfastzombie/fzombie_die2.wav"}
+ENT.MeleeAttackDamage = 5
+ENT.AnimTbl_MeleeAttack = "vjseq_melee"
+ENT.MeleeAttackDistance = 32
+ENT.MeleeAttackDamageDistance = 85
+ENT.TimeUntilMeleeAttackDamage = false
+ENT.NextMeleeAttackTime = 1
+
+ENT.DisableFootStepSoundTimer = true
+---------------------------------------------------------------------------------------------------------------------------------------------
+ENT.SoundTbl_FootStep = {
+	")zsszombie/foot1.wav",
+	")zsszombie/foot2.wav",
+	")zsszombie/foot3.wav",
+	")zsszombie/foot4.wav"
+}
+ENT.SoundTbl_Idle = {
+	")vfastzombie/fzombie_idle1.wav",
+	")vfastzombie/fzombie_idle2.wav",
+	")vfastzombie/fzombie_idle3.wav",
+	")vfastzombie/fzombie_idle4.wav",
+	")vfastzombie/fzombie_idle5.wav"
+}
+ENT.SoundTbl_Alert = {
+	"vfastzombie/fzombie_alert1.wav",
+	"vfastzombie/fzombie_alert2.wav",
+	"vfastzombie/fzombie_alert3.wav"
+}
+ENT.SoundTbl_BeforeMeleeAttack = {
+	"vfastzombie/attack1.wav",
+	"vfastzombie/attack2.wav",
+	"vfastzombie/attack3.wav"
+}
+ENT.SoundTbl_MeleeAttack = {
+	"npc/zombie/claw_strike1.wav",
+	"npc/zombie/claw_strike2.wav",
+	"npc/zombie/claw_strike3.wav"
+}
+ENT.SoundTbl_MeleeAttackMiss = {
+	"zsszombie/miss1.wav",
+	"zsszombie/miss2.wav",
+	"zsszombie/miss3.wav",
+	"zsszombie/miss4.wav"
+}
+ENT.SoundTbl_Pain = {
+	"vfastzombie/fzombie_pain1.wav",
+	"vfastzombie/fzombie_pain2.wav",
+	"vfastzombie/fzombie_pain3.wav"
+}
+ENT.SoundTbl_Death = {
+	"vfastzombie/fzombie_die1.wav",
+	"vfastzombie/fzombie_die2.wav"
+}
+
+ENT.FootStepSoundLevel = 65
 
 ENT.GeneralSoundPitch1 = 100
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
-	self:AddRelationship("npc_headcrab_fast D_LI 99")
-	self:AddRelationship("npc_headcrab_poison D_LI 99")
-	self:SetSkin(math.random(0,3))
+	self:SetSkin( math.random( 0, 3 ) )
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+local getEventName = util.GetAnimEventNameByID
+--
+function ENT:CustomOnHandleAnimEvent( ev )
+	local eventName = getEventName( ev )
+
+	if eventName == "AE_FASTZOMBIE_GALLOP_LEFT" or eventName == "AE_FASTZOMBIE_GALLOP_RIGHT" then
+		self:FootStepSoundCode()
+	elseif eventName == "AE_ZOMBIE_ATTACK_LEFT" or eventName == "AE_ZOMBIE_ATTACK_RIGHT" then
+		self:MeleeAttackCode()
+	end
 end
--- gamemodes/horde/entities/entities/npc_vj_horde_fast_zombie/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_fast_zombie/init.lua
@@ -1,48 +1,86 @@
-AddCSLuaFile("shared.lua")
-include('shared.lua')
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
+--[[---------------------------------------------
+	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------]]
+ENT.Model = "models/zombie/fast.mdl"
+ENT.StartHealth = 40
 
-ENT.Model = {"models/zombie/fast.mdl"}
-ENT.StartHealth = 30
-ENT.VJ_NPC_Class = {"CLASS_ZOMBIE","CLASS_XEN"}
+ENT.VJ_NPC_Class = { "CLASS_ZOMBIE", "CLASS_XEN" }
+
 ENT.BloodColor = "Red"
-ENT.MeleeAttackDamage = 10
-ENT.AnimTbl_MeleeAttack = {"vjseq_br2_attack"}
-ENT.MeleeAttackDistance = 40
-ENT.MeleeAttackDamageDistance = 60
-ENT.TimeUntilMeleeAttackDamage = 0.7
+
+ENT.MeleeAttackDamage = 15
+ENT.AnimTbl_MeleeAttack = "vjseq_br2_attack"
+ENT.MeleeAttackDistance = 32
+ENT.MeleeAttackDamageDistance = 85
+ENT.TimeUntilMeleeAttackDamage = false
+ENT.NextMeleeAttackTime = 1.5
+
 ENT.HasLeapAttack = true
-ENT.AnimTbl_LeapAttack = {"leapstrike"}
-ENT.LeapDistance = 350
-ENT.LeapToMeleeDistance = 150
-ENT.TimeUntilLeapAttackDamage = 0.2
-ENT.NextLeapAttackTime = 5
-ENT.NextAnyAttackTime_Leap = 0.4
-ENT.LeapAttackExtraTimers = {0.4,0.6,0.8,1}
+ENT.AnimTbl_LeapAttack = ACT_JUMP
+ENT.LeapAttackExtraTimers = { 0.2, 0.4, 0.6, 0.8, 1 }
 ENT.TimeUntilLeapAttackVelocity = 0.2
+ENT.LeapDistance = 300
 ENT.LeapAttackVelocityForward = 250
 ENT.LeapAttackVelocityUp = 200
-ENT.LeapAttackDamage = 15
-ENT.LeapAttackDamageDistance = 100
-ENT.FootStepTimeRun = 0.25
-ENT.DisableFootStepOnWalk = true
-ENT.AlertSounds_OnlyOnce = true
-ENT.HasImpactSounds = false
 
-ENT.SoundTbl_FootStep = {"npc/fast_zombie/foot1.wav","npc/fast_zombie/foot2.wav","npc/fast_zombie/foot3.wav","npc/fast_zombie/foot4.wav"}
-ENT.SoundTbl_Idle = {"npc/fast_zombie/idle1.wav","npc/fast_zombie/idle2.wav","npc/fast_zombie/idle3.wav"}
-ENT.SoundTbl_Alert = {"npc/fast_zombie/fz_alert_close1.wav"}
-ENT.SoundTbl_BeforeMeleeAttack = {"npc/fast_zombie/leap1.wav"}
-ENT.SoundTbl_MeleeAttackMiss = {"zsszombie/miss1.wav","zsszombie/miss2.wav","zsszombie/miss3.wav","zsszombie/miss4.wav"}
-ENT.SoundTbl_MeleeAttack = {"npc/zombie/claw_strike1.wav","npc/zombie/claw_strike2.wav","npc/zombie/claw_strike3.wav"}
-ENT.SoundTbl_LeapAttackJump = {"npc/fast_zombie/fz_scream1.wav","npc/fast_zombie/leap1.wav"}
-ENT.SoundTbl_LeapAttackDamage = {"npc/fast_zombie/claw_strike1.wav","npc/fast_zombie/claw_strike2.wav","npc/fast_zombie/claw_strike3.wav"}
-ENT.SoundTbl_Pain = {"npc/fast_zombie/fz_frenzy1.wav","npc/fast_zombie/idle1.wav","npc/fast_zombie/idle2.wav","npc/fast_zombie/idle3.wav"}
-ENT.SoundTbl_Death = {"npc/fast_zombie/wake1.wav"}
+ENT.DisableFootStepSoundTimer = true
+---------------------------------------------------------------------------------------------------------------------------------------------
+ENT.SoundTbl_FootStep = {
+	")zsszombie/foot1.wav",
+	")zsszombie/foot2.wav",
+	")zsszombie/foot3.wav",
+	")zsszombie/foot4.wav"
+}
+ENT.SoundTbl_Idle = {
+	"npc/fast_zombie/idle1.wav",
+	"npc/fast_zombie/idle2.wav",
+	"npc/fast_zombie/idle3.wav"
+}
+ENT.SoundTbl_Alert = "npc/fast_zombie/fz_alert_close1.wav"
+ENT.SoundTbl_BeforeMeleeAttack = "npc/fast_zombie/leap1.wav"
+ENT.SoundTbl_MeleeAttack = {
+	"npc/zombie/claw_strike1.wav",
+	"npc/zombie/claw_strike2.wav",
+	"npc/zombie/claw_strike3.wav"
+}
+ENT.SoundTbl_MeleeAttackMiss = {
+	"zsszombie/miss1.wav",
+	"zsszombie/miss2.wav",
+	"zsszombie/miss3.wav",
+	"zsszombie/miss4.wav"
+}
+ENT.SoundTbl_LeapAttackJump = {
+	"npc/fast_zombie/fz_scream1.wav",
+	"npc/fast_zombie/leap1.wav"
+}
+ENT.SoundTbl_LeapAttackDamage = {
+	"npc/zombie/claw_strike1.wav",
+	"npc/zombie/claw_strike2.wav",
+	"npc/zombie/claw_strike3.wav"
+}
+ENT.SoundTbl_Pain = "npc/fast_zombie/fz_frenzy1.wav"
+ENT.SoundTbl_Death = "npc/fast_zombie/wake1.wav"
+
+ENT.FootStepSoundLevel = 65
 
 ENT.GeneralSoundPitch1 = 100
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
-	self:SetBodygroup(1,1)
-	self:AddRelationship("npc_headcrab_fast D_LI 99")
-	self:AddRelationship("npc_headcrab_poison D_LI 99")
+	self:SetBodygroup( 1, 1 )
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+local getEventName = util.GetAnimEventNameByID
+--
+function ENT:CustomOnHandleAnimEvent( ev )
+	local eventName = getEventName( ev )
+
+	if eventName == "AE_FASTZOMBIE_GALLOP_LEFT" or eventName == "AE_FASTZOMBIE_GALLOP_RIGHT" then
+		self:FootStepSoundCode()
+	elseif eventName == "AE_ZOMBIE_ATTACK_LEFT" or eventName == "AE_ZOMBIE_ATTACK_RIGHT" then
+		self:MeleeAttackCode()
+	end
 end
