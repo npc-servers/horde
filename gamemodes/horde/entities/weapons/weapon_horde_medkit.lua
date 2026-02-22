a56aacf0321a8d788020fd8fe03b95f22b45ed9e
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-02-21 18:47:22 -0800

    fix(revive xp): Half mult instead of base xp

--- gamemodes/horde/entities/weapons/weapon_horde_medkit.lua
+++ gamemodes/horde/entities/weapons/weapon_horde_medkit.lua
@@ -436,9 +436,9 @@ if SERVER then
 		if owner:Horde_GetLevel( subclass ) < maxLevel then
 			local wavePercent = HORDE.current_wave / HORDE.max_waves
 			local roundXpMulti = startXpMult + ( wavePercent * endMinusStartXp ) -- This gets the xp multi number between min and max multi based on round
-			local expMulti = roundXpMulti * expMultiConvar:GetInt()
+			local expMulti = roundXpMulti * expMultiConvar:GetInt() / 2
 
-			owner:Horde_GiveExp( subclass, 12 * expMulti, "Reviving a Player" )
+			owner:Horde_GiveExp( subclass, 25 * expMulti, "Reviving a Player" )
 		end
 
 		net.Start( "horde_medkit_player_revived" )
