98067a752b05be824283b51006f9417bf1e04e08
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-02-28 11:10:33 -0800

    XP Balance Changes (#397)
    * fix(armor): nerf XP percentage + nerf mult
    
    * fix(healing): nerf mult
    
    * fix(xp): nerf ammobox xp

--- gamemodes/horde/gamemode/sv_heal.lua
+++ gamemodes/horde/gamemode/sv_heal.lua
@@ -44,8 +44,8 @@ local startXpMult = HORDE.Difficulty[HORDE.CurrentDifficulty].xpMultiStart
 local endXpMult = HORDE.Difficulty[HORDE.CurrentDifficulty].xpMultiEnd
 local endMinusStartXp = endXpMult - startXpMult
 local maxLevel = HORDE.max_level
-local healXpPercentage = 0.25
-local armorXpPercentage = 0.25
+local healXpPercentage = 0.02
+local armorXpPercentage = 0.02
 
 function plymeta:Horde_AddHealAmount( amount )
     if HORDE.current_wave <= 0 then return end
@@ -56,7 +56,7 @@ function plymeta:Horde_AddHealAmount( amount )
 
     local wavePercent = HORDE.current_wave / HORDE.max_waves
     local roundXpMult = startXpMult + ( wavePercent * endMinusStartXp ) -- This gets the xp multi number between min and max multi based on round
-    local expMult = roundXpMult * expMultiConvar:GetInt()
+    local expMult = roundXpMult * expMultiConvar:GetInt() / 2
 
     self:Horde_GiveExp( subclass, healXpPercentage * amount * expMult, "Healed Player" )
 end
@@ -147,9 +147,11 @@ local function armorerDoGiveXp( armorer, armorGiven )
 
     local wavePercent = HORDE.current_wave / HORDE.max_waves
     local roundXpMult = startXpMult + wavePercent * endMinusStartXp
-    local expMult = roundXpMult * expMultiConvar:GetInt()
+    local expMult = roundXpMult * expMultiConvar:GetInt() / 2
 
-    armorer:Horde_GiveExp( subclass, armorXpPercentage * armorGiven * expMult, "Provided Armor" )
+    local rewardMult = armorXpPercentage * armorGiven * math.max( 1, expMult )
+
+    armorer:Horde_GiveExp( subclass, rewardMult, "Provided Armor" )
 end
 
 function plymeta:Horde_GiveArmor( armorAmount, armorer )
