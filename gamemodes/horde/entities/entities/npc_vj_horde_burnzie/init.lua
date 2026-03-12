d4bfc086b786c2e11317819fc1c83bb69ac12e53
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2026-03-12 00:07:15 -0400

    burnzie+eschar hitbox fixes
    fixed and cleaned up a bunch of hitboxes on both the burnzie and eschar, also gave them head hitgroups on their head hitboxes

--- gamemodes/horde/entities/entities/npc_vj_horde_burnzie/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_horde_burnzie/init.lua
@@ -5,7 +5,7 @@ include( "shared.lua" )
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------]]
-ENT.Model = { "models/zombie/burnzie.mdl" }
+ENT.Model = { "models/horde/zombie/burnzie.mdl" }
 ENT.StartHealth = 150
 
 ENT.VJ_NPC_Class = { "CLASS_ZOMBIE", "CLASS_XEN" }
