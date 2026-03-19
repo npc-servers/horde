98ce57ce0465253d321eefdc162c35d229bb766c
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2026-03-19 16:36:21 -0400

    money cap to prevent overflow
    capped the amount of money a player can have to prevent players from overflowing their money and getting set back to 0 again (this has happened already, someone actually managed to overflow like twice in one match)

--- gamemodes/horde/gamemode/sv_economy.lua
+++ gamemodes/horde/gamemode/sv_economy.lua
@@ -178,7 +178,11 @@ end
 function plymeta:Horde_AddMoney(money)
     if not self:IsValid() or not money then return end
     if not self.Horde_money then self.Horde_money = 0 end
-    self.Horde_money = self.Horde_money + money
+    if self.Horde_money + money >= 65535 then
+        self.Horde_money = 65535
+    else
+        self.Horde_money = self.Horde_money + money
+    end
 end
 
 function plymeta:Horde_AddWeight(weight)
