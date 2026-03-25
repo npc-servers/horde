a1a48374750f7cad03ca3c0cac1b03353ee69773
func_brush <jacobamadolopez2006@gmail.com>
2026-03-24 21:45:51 -0700

    fixes

--- gamemodes/horde/gamemode/sh_item.lua
+++ gamemodes/horde/gamemode/sh_item.lua
@@ -431,19 +431,19 @@ function HORDE:GetDefaultItemsData()
         { Berserker = true, Samurai = true, ["Cyborg Ninja"] = true },
         10, -1, nil, nil, { Berserker = 5 }, nil, { HORDE.DMG_SLASH } )
 
-    HORDE:CreateItem( "Pistol", "M1911", "arccw_horde_bo_1911", 10, 1,
+    HORDE:CreateItem( "Pistol", "M1911 Starter", "arccw_horde_bo_1911", 10, 1,
         "Classic starter weapon for any zombie environment.",
         { Survivor = true, Psycho = true, Prototype = true, Demolition = true, Ghost = true, Gunslinger = true, Engineer = true, Warden = true, Overlord = true, Cremator = true },
         2, -1, nil, "items/hl2/weapon_pistol.png", nil, nil, { HORDE.DMG_BALLISTIC }, nil, { "Survivor", "Prototype", "Psycho", "Demolition", "Ghost", "Gunslinger", "Engineer", "Warden", "Overlord", "Cremator" } )
-    HORDE:CreateItem( "Pistol", "ASP", "arccw_horde_bo_asp", 10, 1,
+    HORDE:CreateItem( "Pistol", "ASP Starter", "arccw_horde_bo_asp", 10, 1,
         "Low profile pistol.\nThis one is chambered in a special cartridge, designed for melee fighters.",
         { Berserker = true, Samurai = true, ["Cyborg Ninja"] = true },
         2, -1, nil, "items/hl2/weapon_pistol.png", nil, nil, { HORDE.DMG_SLASH }, nil, { "Berserker", "Samurai", "Cyborg Ninja" } )
-    HORDE:CreateItem( "Pistol", "CZ75", "arccw_horde_bo_cz75", 10, 1,
+    HORDE:CreateItem( "Pistol", "CZ75 Starter", "arccw_horde_bo_cz75", 10, 1,
         "Fully automatic pistol.",
         { Assault = true, SpecOps = true, Reverend = true, Heavy = true, Juggernaut = true },
         2, -1, nil, "items/hl2/weapon_pistol.png", nil, nil, { HORDE.DMG_BALLISTIC }, nil, { "Assault", "SpecOps", "Reverend", "Heavy", "Juggernaut" } )
-    HORDE:CreateItem( "Pistol", "Makarov", "arccw_horde_bo_makarov", 10, 1,
+    HORDE:CreateItem( "Pistol", "Makarov Starter", "arccw_horde_bo_makarov", 10, 1,
         "Standard pistol of Soviet design.\nThis one is loaded with healing darts.",
         { Medic = true, Hatcher = true },
         2, -1, nil, "items/hl2/weapon_pistol.png", nil, nil, { HORDE.DMG_BALLISTIC }, nil, { "Medic", "Hatcher" } )
