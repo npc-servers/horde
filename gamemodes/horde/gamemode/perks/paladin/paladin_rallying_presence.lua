3e466d8a38835c1e9276a689b726abed49f41524
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-03-27 08:17:29 -0700

    Paladin (#386)
    * Init Paladin
    
    * Start working on Aura
    
    * Aura size now increases based on paladin levels instead of overlord
    
    * Add more passives to base
    
    * add divine shield
    
    * remove print
    
    * Shield Bash
    
    * New desc
    
    * Update paladin icon
    
    * Fix faith stacks not regening till after hitting alt
    
    * move Horde_PostOnPlayerHeal to paladin base
    
    * actually use icon path + add status smite
    
    * Forgot to rename smite to smite
    
    * Give items to Paladin
    
    * Add Smite + Empowered status
    
    * Protectors Oath
    
    * Prevent exploit
    
    * Fix wrong hook
    
    * Inquisitor's Oath
    
    * Dawnbrinder
    
    * change var location
    
    * make it more like a second then half a second
    
    * Fix not being 50%
    
    * Untested Providence
    
    * Fix wrong hook
    
    * Sanctuary
    
    * Skip protectors if self has sanctuary
    
    * Rallying Presence
    
    * Give paladin warden's gadgets
    
    * Give Paladin EMP grenade
    
    * Remove regen
    
    * New icons
    
    * faith icon
    
    * placeholder text
    
    * Remove dmg checks, Add comment
    
    * stun enemies + optimize code slightly
    
    * Added regen
    
    * fix regen
    
    * Fix armor being regened
    
    * fix error
    
    * start reworking
    
    * remove Horde_PaladinIsEntityInsideAura
    
    * fix
    
    * fix
    
    * update desc
    
    * optimize code slightly
    
    * swap to add, rename timer, helper functions for timer
    
    * styling
    
    * precompute level bonus
    
    * rename function and rename vars
    
    * Fixed not removing aura effects
    
    * Fix paladin aura icon not appearing
    
    * Add shielding status, moved helper function
    
    * Compact desc
    
    * Styling fix
    
    * Fix Styling
    
    * styling fix
    
    * Styling Fixes
    
    * give aura status after a tick
    
    * add owner to aura ents table
    
    * fix smite
    
    * Remove debuffs
    
    * correctly use stun debuff
    
    * add guard clauses, correctly add faith regen
    
    * Removed after dmg faith removal (too strong)
    
    * math wrong
    
    * fix for rework, optimized slightly
    
    * remove debug print
    
    * optimized + prevent resist from being given while not shielding
    
    * Fix providence
    
    * Fix dmg type and dmg
    
    * Optimize
    
    * Fix dmg type and paladin_dawnbrinder not working
    
    * fix wrong perk, fix insideAura
    
    * Remove shielding required
    
    * Fix wrong hook, readd helper
    
    * Filewide server check
    
    * optimize
    
    * optimze isElementalDamage and add server checks
    
    * add server checks, fix rallying presence
    
    * rename function, get correct perk
    
    * Heavily Optimize think hook
    
    * optimize inside aura code
    
    * Added Antispam
    
    * Fix inverted logic
    
    * Correctly set cooldown
    
    * use smite icon as empowered
    
    * Prevent paladins from shielding each other, Prevent Providence shielding (except reflect) from working on other paladins
    
    * Added faith scaling for elemental resist
    
    * Replace placeholder section titles
    
    * Reduce dmg increase
    
    * Change dmg to 100
    
    * nerf smite dmg by 100
    
    * Stop running while shielding
    
    * add screen effect
    
    * Adding dashing shield for shield bashing
    
    * Add smite empowered sound
    
    * Add shield sound
    
    * Faith loss effect + sound
    
    * Fix non-melee hits leeching
    
    * Fix bonus dmg %
    
    * revert smite and shield bash to old values
    
    * Nerf damage boost to be melee only
    
    * All shielding players lose faith now
    
    * updated description
    
    * update description
    
    * buff dawnbringer DOT
    
    * melee dmg not physical
    
    * Fix error
    
    * buff
    
    * buff protector's oath
    
    * prevent any possible issues
    
    * Smite effect
    
    * prevent any possible issues
    
    * prevent any possible issues
    
    * prevent hit sound from being loud af + styling
    
    * add shield bash effect
    
    * fix(shielding): logic bug
    
    * fix shielding visual bug
    
    * styling: add spaces inbetween parentheses
    
    * styling
    
    * allow for rebinding of shield key
    
    * global add/remove shield status
    
    * fix directional audio
    
    * remove unneeded comment
    
    * Fix grammar
    
    * audio fix
    
    * audio fix
    
    * fix spaces
    
    * audio fix
    
    * audio fix
    
    * fixed

---
+++ gamemodes/horde/gamemode/perks/paladin/paladin_rallying_presence.lua
@@ -0,0 +1,40 @@
+PERK.PrintName = "Rallying Presence"
+PERK.Icon = "materials/perks/paladin/rallying_presence.png"
+PERK.Description = [[
+All players inside Sacred Aura deal 20% more damage when their health is full.
+All players inside Sacred Aura regenerate 2% health if their health is lower than full.]]
+PERK.Hooks = {}
+
+if not SERVER then return end
+
+local function insideAura( ply, insideAuraPly )
+    local aura = ply.Horde_PaladinAura
+    if not aura then return false end
+
+    local entsInside = aura.Entities
+    if not entsInside or not entsInside[insideAuraPly] then return false end
+
+    return true
+end
+
+local function isProtected( ply )
+    local players = player.GetAll()
+
+    for i = 1, #players do
+        local auraPly = players[i]
+        if auraPly:Horde_GetPerk( "paladin_rallying_presence" ) and insideAura( auraPly, ply ) then
+            return true
+        end
+    end
+
+    return false
+end
+
+local dmgPercentIncrease = 0.20
+
+PERK.Hooks.Horde_OnPlayerDamage = function( ply, _, bonus )
+    if ply:Health() < ply:GetMaxHealth() then return end
+    if not ply:Horde_GetPerk( "paladin_rallying_presence" ) and not isProtected( ply ) then return end
+
+    bonus.increase = bonus.increase + dmgPercentIncrease
+end
