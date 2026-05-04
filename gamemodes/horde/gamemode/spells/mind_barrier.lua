a0b5be2b1b7d71c50f415e2aee15cd92bb56e5e9
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-05-04 13:34:05 -0700

    refactor(mind barrier): AoE, upgradable, effects (#443)

--- gamemodes/horde/gamemode/spells/mind_barrier.lua
+++ gamemodes/horde/gamemode/spells/mind_barrier.lua
@@ -1,14 +1,39 @@
 SPELL.PrintName      = "Mind Barrier"
-SPELL.Mind           = {10}
-SPELL.ChargeTime     = {0}
+SPELL.Mind           = { 10 }
+SPELL.ChargeTime     = { 1.5 }
 SPELL.ChargeRelease  = nil
 SPELL.Cooldown       = 10
+SPELL.Levels         = { Hatcher = 5 }
+SPELL.Price          = 600
+SPELL.Upgrades       = 2
+SPELL.Upgrade_Prices = function()
+    return 600
+end
 SPELL.Slot           = HORDE.Spell_Slot_Utility
 SPELL.Icon           = "spells/barrier.png"
-SPELL.Type           = {HORDE.Spell_Type_Utility}
-SPELL.Description    = [[Grants 20 Barrier to the user.]]
-SPELL.Fire           = function (ply, wpn, charge_stage)
-    ply:Horde_AddBarrierStack(20)
+SPELL.Type           = { HORDE.Spell_Type_Utility }
+SPELL.Description    = "Grants 20 Barrier in a 160hu radius around the caster."
+SPELL.Upgrade_Description = "Increases barrier given by 15."
+
+SPELL.Fire = function( ply )
+    local level = ply:Horde_GetSpellUpgrade( "mind_barrier" )
+    local barrier = 20 + ( 15 * level )
+    local plyPos = ply:GetPos()
+
+    ply:EmitSound( "horde/player/life_diffuser.ogg", 100, 100, 1, CHAN_AUTO )
+
+    local effectdata = EffectData()
+    effectdata:SetOrigin( plyPos )
+    effectdata:SetRadius( 160 )
+    util.Effect( "horde_mind_barrier", effectdata, nil, true )
+
+    local nearbyEnts = ents.FindInSphere( plyPos, 160 )
+
+    for i = 1, #nearbyEnts do
+        local ent = nearbyEnts[i]
+
+        if ent:IsPlayer() then
+            ent:Horde_AddBarrierStack( barrier )
+        end
+    end
 end
-SPELL.Price          = 600
-SPELL.Levels          = {Artificer=5, Warlock=5, Necromancer=5}
