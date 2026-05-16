4850051f6ac4b7cf5c000426770836a9183af870
hazel <bibpkin@gmail.com>
2026-05-16 18:57:29 -0400

    even more glorpshit

--- gamemodes/horde/gamemode/subclasses/spellsword.lua
+++ gamemodes/horde/gamemode/subclasses/spellsword.lua
@@ -7,8 +7,8 @@ Berserker subclass.
 A slow caster that uses destructive spells.]] -- Required
 SUBCLASS.BasePerk = "spellsword_base"
 SUBCLASS.Perks = {
-    [1] = {title = "Study", choices = {"spellsword_l1p", "spellsword_r1p"}},
-    [2] = {title = "Channelling", choices = {"spellsword_l2p", "spellsword_r2p"}},
-    [3] = {title = "Vectorize", choices = {"spellsword_l3p", "spellsword_r3p"}},
-    [4] = {title = "Annihilation", choices = {"spellsword_l4p", "spellsword_r4p"}},
+    [1] = {title = "Research", choices = {"spellsword_l1p", "spellsword_r1p"}},
+    [2] = {title = "Strategy", choices = {"spellsword_l2p", "spellsword_r2p"}},
+    [3] = {title = "Scholarship", choices = {"spellsword_l3p", "spellsword_r3p"}},
+    [4] = {title = "Enlightenment", choices = {"spellsword_l4p", "spellsword_r4p"}},
 } -- Required
