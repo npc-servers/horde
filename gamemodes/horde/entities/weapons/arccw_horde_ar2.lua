789b39468e3fd1780ff0f11c04a72fed13380e4d
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-07-07 20:31:25 -0700

    fix: misplaced comma

--- gamemodes/horde/entities/weapons/arccw_horde_ar2.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_ar2.lua
@@ -163,7 +163,7 @@ SWEP.Attachments = {
 }
 
 SWEP.Animations = {
-    ["idle"] = { Source = "IR_idle", Time = 10, }
+    ["idle"] = { Source = "IR_idle", Time = 10 },
     ["draw"] = {
         Source = "IR_draw",
         FrameRate = 30,
