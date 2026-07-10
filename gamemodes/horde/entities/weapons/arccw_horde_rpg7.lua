a8910305e501ea514fa13b20a3aa5c10825bbcc7
Skefil <69618043+Joeygi@users.noreply.github.com>
2026-07-09 18:28:27 -0700

    Projection Clone description update, OSIPR sound/animation fix, RPG reload sound fix, New OSIPR muzzle FX (#473)
    * update pclone description
    
    holo-goons uses auto shotguns now, so description updated
    
    * description update
    
    same with the gadget one
    
    * missed something
    
    got late
    
    * rocket launchers
    
    fixed the indentation (linter was angy) alongside fixed sound, probably missing something
    
    * ar2 content
    
    added the sounds and anims, also gave it the hunter muzzleflash, if it's too grating i'll revert it
    
    * changed firing events
    
    changed the primary fire event to it's default one, alongside the ads animation
    
    * restored new sound, multiline change
    
    pr was made before the sfx changes,
    
    * probably missing something again
    
    * ow my spine DXC
    
    * i'm stupid
    
    why am i still using the github editor i should be using vscode from now on
    
    * welle
    
    * removed that
    
    * style: fix table indentation
    
    * idle
    
    * still wrong
    
    * unintentional
    
    * hopefully that's it
    
    * fix: misplaced comma
    
    * style: fix minor styling issues
    
    * chore: remove unused code
    
    * swapped to mk2 raygun flash
    
    apparently gmod ep2 content doesn't actually work despite it coming with base gmod, makes no sense
    
    * let's try this
    
    ---------
    
    Co-authored-by: Alex9914 <77191624+Alex9914@users.noreply.github.com>

--- gamemodes/horde/entities/weapons/arccw_horde_rpg7.lua
+++ gamemodes/horde/entities/weapons/arccw_horde_rpg7.lua
@@ -1,10 +1,10 @@
 if not ArcCWInstalled then return end
-if (CLIENT) then
-	SWEP.WepSelectIcon = surface.GetTextureID("vgui/hud/arccw_horde_rpg7")
+if CLIENT then
+    SWEP.WepSelectIcon = surface.GetTextureID( "vgui/hud/arccw_horde_rpg7" )
     SWEP.DrawWeaponInfoBox	= false
     SWEP.BounceWeaponIcon = false
-	killicon.Add("arccw_horde_rpg7", "vgui/hud/arccw_horde_rpg7", color_white)
-    killicon.Add("horde_projectile_rpg", "vgui/hud/arccw_horde_rpg7", color_white)
+    killicon.Add( "arccw_horde_rpg7", "vgui/hud/arccw_horde_rpg7", color_white )
+    killicon.Add( "horde_projectile_rpg", "vgui/hud/arccw_horde_rpg7", color_white )
 end
 SWEP.Base = "arccw_base"
 SWEP.Spawnable = true -- this obviously has to be set to true
@@ -34,8 +34,8 @@ SWEP.DefaultBodygroups = "00000000"
 SWEP.MirrorVMWM = true -- Copy the viewmodel, along with all its attachments, to the worldmodel. Super convenient!
 
 SWEP.WorldModelOffset = {
-    pos = Vector(-16.5, 5.5, -8),
-    ang = Angle(-5, 0, 180),
+    pos = Vector( -16.5, 5.5, -8 ),
+    ang = Angle( -5, 0, 180 ),
 }
 SWEP.ViewModelFOV = 60
 
@@ -50,7 +50,7 @@ SWEP.MuzzleVelocity = 2000 -- projectile or phys bullet muzzle velocity
 SWEP.CanFireUnderwater = false
 
 SWEP.TracerNum = 1 -- tracer every X
-SWEP.TracerCol = Color(255, 25, 25)
+SWEP.TracerCol = Color( 255, 25, 25 )
 SWEP.TracerWidth = 3
 
 SWEP.ChamberSize = 0 -- how many rounds can be chambered.
@@ -66,12 +66,8 @@ SWEP.RecoilRise = 1
 SWEP.Delay = 60 / 100 -- 60 / RPM.
 SWEP.Num = 1 -- number of shots per trigger pull.
 SWEP.Firemodes = {
-    {
-        Mode = 2,
-    },
-    {
-        Mode = 0
-    }
+    { Mode = 2 },
+    { Mode = 0 },
 }
 
 SWEP.AccuracyMOA = 10 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
@@ -100,8 +96,8 @@ SWEP.ProceduralRegularFire = false
 SWEP.ProceduralIronFire = false
 
 SWEP.IronSightStruct = {
-    Pos = Vector(-3.12, 0, 1.8),
-    Ang = Angle(-0, 0, 0),
+    Pos = Vector( -3.12, 0, 1.8 ),
+    Ang = Angle( -0, 0, 0 ),
     Magnification = 1.3,
 }
 
@@ -111,25 +107,23 @@ SWEP.HoldtypeSights = "rpg"
 
 SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
 
-SWEP.ActivePos = Vector(0, 0, 0)
-SWEP.ActiveAng = Angle(0, 0, 0)
+SWEP.ActivePos = Vector( 0, 0, 0 )
+SWEP.ActiveAng = Angle( 0, 0, 0 )
 
-SWEP.HolsterPos = Vector(-2, -7.145, -11.561)
-SWEP.HolsterAng = Angle(36.533, 0, 0)
+SWEP.HolsterPos = Vector( -2, -7.145, -11.561 )
+SWEP.HolsterAng = Angle( 36.533, 0, 0 )
 
-SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
-SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+SWEP.BarrelOffsetSighted = Vector( 0, 0, -1 )
+SWEP.BarrelOffsetHip = Vector( 2, 0, -2 )
 
-SWEP.CustomizePos = Vector(12, -8, -4.897)
-SWEP.CustomizeAng = Angle(12.149, 30.547, 0)
+SWEP.CustomizePos = Vector( 12, -8, -4.897 )
+SWEP.CustomizeAng = Angle( 12.149, 30.547, 0 )
 
-SWEP.AttachmentElements = {
-
-}
+SWEP.AttachmentElements = {}
 
 SWEP.ExtraSightDist = 5
 
-SWEP.RejectAttachments = {["go_homemade_auto"] = true, ["go_perk_burst"] = true}
+SWEP.RejectAttachments = { ["go_homemade_auto"] = true, ["go_perk_burst"] = true }
 SWEP.Attachments = {
     {
         PrintName = "Ammo",
@@ -151,8 +145,8 @@ SWEP.Animations = {
         Time = 0.1,
         SoundTable = {
             {
-            s = "weapons/arccw/draw_secondary.wav",
-            t = 0
+                s = "weapons/arccw/draw_secondary.wav",
+                t = 0
             }
         },
         LHIK = true,
@@ -164,13 +158,33 @@ SWEP.Animations = {
         Time = 0.5,
         ShellEjectAt = 0,
         RestoreAmmo = 1,
-        SoundTable = { { s = "", p = 100, v = 75, t = 0.1, c = CHAN_ITEM, ind = 1, bg = 1, } },
+        SoundTable = {
+            {
+                s = "",
+                p = 100,
+                v = 75,
+                t = 0.1,
+                c = CHAN_ITEM,
+                ind = 1,
+                bg = 1,
+            }
+        },
     },
     ["fire_iron"] = {
         Source = "shoot",
         Time = 0.5,
         ShellEjectAt = 0,
-        SoundTable = { {  s = "", p = 100, v = 75, t = 0.1, c = CHAN_ITEM, ind = 1, bg = 1, } },
+        SoundTable = {
+            {
+                s = "",
+                p = 100,
+                v = 75,
+                t = 0.1,
+                c = CHAN_ITEM,
+                ind = 1,
+                bg = 1,
+            }
+        },
     },
     ["reload"] = {
         Source = "reload",
@@ -180,7 +194,17 @@ SWEP.Animations = {
         LHIK = true,
         LHIKIn = 0.5,
         LHIKOut = 0.2,
-        SoundTable = { { s = "", p = 100, v = 75, t = 1.8, c = CHAN_ITEM, ind = 1, bg = 0, }, },
+        SoundTable = {
+            {
+                s = "weapons/ar2/ar2_reload.wav",
+                p = 100,
+                v = 75,
+                t = 0,
+                c = CHAN_ITEM,
+                ind = 1,
+                bg = 0,
+            },
+        },
     },
 }
 
