9651eb2123a9dd8d7ec38de46a8a4bbb8bdd80b3
Uklejamini357 <uklejamini@tutamail.com>
2026-08-05 14:08:12 +0200

    avoid tip panel overlapping with ready (move it a bit below player ready list panel)

--- gamemodes/horde/gamemode/gui/cl_ready.lua
+++ gamemodes/horde/gamemode/gui/cl_ready.lua
@@ -71,6 +71,7 @@ local tip = ""
 HORDE.TipPanel = vgui.Create("DPanel")
 HORDE.TipPanel:SetSize(ScrW() * 2 / 5, ScreenScale(15))
 HORDE.TipPanel:SetPos(ScrW() / 2 - ScrW() * 2 / 10, ScreenScale(6))
+HORDE.TipPanel:MoveBelow(HORDE.PlayerReadyPanel, ScreenScale(2))
 HORDE.TipPanel.Paint = function (w,h)
     if tip == nil or tip == "" then return end
     draw.RoundedBox(10, 0, 0, ScrW() * 2 / 5, ScreenScale(15),  Color(40,40,40,200))
