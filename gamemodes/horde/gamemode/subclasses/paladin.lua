7f5cb2ca4e8f01d4742e5d9af0c89af81a1ad544
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-08-08 09:45:57 -0700

    feat: rework providence (now renamed bastion)
    finally this pain in the ass perk is gone

--- gamemodes/horde/gamemode/subclasses/paladin.lua
+++ gamemodes/horde/gamemode/subclasses/paladin.lua
@@ -10,6 +10,6 @@ SUBCLASS.BasePerk = "paladin_base"
 SUBCLASS.Perks = {
     [1] = { title = "Enlightment", choices = { "paladin_shield_bash", "paladin_smite" } },
     [2] = { title = "Pledge", choices = { "paladin_oath_of_a_shield", "paladin_oath_of_a_sword" } },
-    [3] = { title = "Valour", choices = { "paladin_providence", "paladin_excalibur" } },
+    [3] = { title = "Valour", choices = { "paladin_bastion", "paladin_excalibur" } },
     [4] = { title = "Beacon of Hope", choices = { "paladin_burning_passion", "paladin_judgement" } },
 }
