5d884e89bfc6f1f0bb509d3c1d86b18f84ffaf6e
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-08-08 02:23:09 -0700

    feat: rework smite

--- gamemodes/horde/gamemode/perks/paladin/paladin_smite.lua
+++ gamemodes/horde/gamemode/perks/paladin/paladin_smite.lua
@@ -1,9 +1,9 @@
 PERK.PrintName = "Smite"
 PERK.Icon = "materials/perks/paladin/smite.png"
 PERK.Description = [[
-Press Shift + E to make your next melee hit deal additional 200 AOE 
-Lightning damage, healing nearby allies by 20% health and clearing 
-all debuffs from them. Cooldown: 10 seconds.]]
+Press Shift + E to unleash a shockwave, with 8 seconds cooldown.
+Enemies in your aura take 100 Lightning damage and Shock debuff. 
+Allies in your aura are healed for 20% health, removing their debuffs.]]
 PERK.Hooks = {}
 
 if not SERVER then return end
@@ -12,7 +12,7 @@ PERK.Hooks.Horde_OnSetPerk = function( ply, perk )
     if perk ~= "paladin_smite" then return end
 
     if not ply:Horde_GetPerk( "paladin_dawnbrinder" ) then
-        ply:Horde_SetPerkCooldown( 10 )
+        ply:Horde_SetPerkCooldown( 8 )
     end
 
     net.Start( "Horde_SyncActivePerk" )
@@ -28,32 +28,14 @@ PERK.Hooks.Horde_OnUnsetPerk = function( ply, perk )
         net.WriteUInt( HORDE.Status_PaladinSmite, 8 )
         net.WriteUInt( 0, 3 )
     net.Send( ply )
-
-    ply.Horde_PaladinEmpowered = nil
-    net.Start( "Horde_SyncActivePerk" )
-        net.WriteUInt( HORDE.Status_PaladinEmpowered, 8 )
-        net.WriteUInt( 0, 3 )
-    net.Send( ply )
 end
 
-local dmgAmt = 200
+local healPercentage = 0.2
+local dmgAmt = 100
 local dmgType = DMG_SHOCK
 
-PERK.Hooks.Horde_OnPlayerDamagePost = function( ply, npc, _, _, dmginfo )
+PERK.Hooks.Horde_UseActivePerk = function( ply )
     if not ply:Horde_GetPerk( "paladin_smite" ) then return end
-    if not ply.Horde_PaladinEmpowered then return end
-    if not HORDE:IsMeleeDamage( dmginfo ) then return end
-
-    ply.Horde_PaladinEmpowered = nil
-
-    net.Start( "Horde_SyncActivePerk" )
-        net.WriteUInt( HORDE.Status_PaladinEmpowered, 8 )
-        net.WriteUInt( 0, 3 )
-    net.Send( ply )
-
-    if IsValid( npc ) then
-        npc:Horde_AddDebuffBuildup( HORDE.Status_Shock, 1000, ply, npc:GetPos() )
-    end
 
     local aura = ply.Horde_PaladinAura
     if not aura then return end
@@ -61,32 +43,26 @@ PERK.Hooks.Horde_OnPlayerDamagePost = function( ply, npc, _, _, dmginfo )
     local entsInside = aura.Entities
     if not entsInside then return end
 
-    local healPercent = 0.2
-    local damage = dmgAmt
-
-    if ply:Horde_GetPerk( "paladin_dawnbrinder" ) then
-        damage = damage * 2
-    end
-
-    local smiteDmg = DamageInfo()
-    smiteDmg:SetDamageType( dmgType )
-    smiteDmg:SetAttacker( ply )
-    smiteDmg:SetInflictor( ply )
-    smiteDmg:SetDamage( damage )
-
-    local effectPos = npc and npc:GetPos() or dmginfo:GetDamagePosition()
-
     local effect = EffectData()
-        effect:SetOrigin( effectPos )
-        effect:SetRadius( 100 )
+    effect:SetOrigin( ply:GetPos() )
+    effect:SetRadius( 300 )
+
     util.Effect( "paladin_smite", effect, true, true )
+    util.Effect( "stun_flash", effect, true, true )
 
     for ent, _ in pairs( entsInside ) do
         if HORDE:IsEnemy( ent ) then
+            local smiteDmg = DamageInfo()
+            smiteDmg:SetDamageType( dmgType )
+            smiteDmg:SetAttacker( ply )
+            smiteDmg:SetInflictor( ply )
+            smiteDmg:SetDamage( dmgAmt )
             smiteDmg:SetDamagePosition( ent:GetPos() )
+
             ent:TakeDamageInfo( smiteDmg )
+            ent:Horde_AddDebuffBuildup( HORDE.Status_Shock, 1000, ply, ent:GetPos() )
         elseif ent:IsPlayer() then
-            local healinfo = HealInfo:New( { amount = ent:GetMaxHealth() * healPercent, healer = ply } )
+            local healinfo = HealInfo:New( { amount = ent:GetMaxHealth() * healPercentage, healer = ply } )
             HORDE:OnPlayerHeal( ent, healinfo )
 
             for debuff, _ in pairs( ent.Horde_Debuff_Buildup ) do
@@ -95,17 +71,4 @@ PERK.Hooks.Horde_OnPlayerDamagePost = function( ply, npc, _, _, dmginfo )
             end
         end
     end
-end
-
-PERK.Hooks.Horde_UseActivePerk = function( ply )
-    if not ply:Horde_GetPerk( "paladin_smite" ) then return end
-
-    ply.Horde_PaladinEmpowered = true
-
-    ply:EmitSound( ")horde/spells/solar_storm_charge.ogg" )
-
-    net.Start( "Horde_SyncActivePerk" )
-        net.WriteUInt( HORDE.Status_PaladinEmpowered, 8 )
-        net.WriteUInt( 1, 3 )
-    net.Send( ply )
 end
