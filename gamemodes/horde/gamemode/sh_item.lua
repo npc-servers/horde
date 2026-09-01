049235549b0b7cfbaaf776b29d3bc58640101df7
Skefil <69618043+Joeygi@users.noreply.github.com>
2026-08-31 21:05:28 -0700

    Standard nailgun infusions, Nailshot price/weight increase

--- gamemodes/horde/gamemode/sh_item.lua
+++ gamemodes/horde/gamemode/sh_item.lua
@@ -295,6 +295,7 @@ function HORDE:GetDefaultItemInfusions()
     HORDE.items["arccw_horde_mp40"].infusions = ballistic_infusions_smgs
     HORDE.items["arccw_horde_uzi"].infusions = ballistic_infusions_smgs
     HORDE.items["arccw_horde_mp5"].infusions = ballistic_infusions_smgs
+    HORDE.items["arccw_horde_nailgun"].infusions = melee_slash_infusions
     HORDE.items["arccw_horde_ump"].infusions = ballistic_infusions_smgs
     HORDE.items["arccw_horde_bizon"].infusions = ballistic_infusions_smgs
     HORDE.items["arccw_horde_p90"].infusions = ballistic_infusions_smgs
@@ -629,7 +630,7 @@ function HORDE:GetDefaultItemsData()
         "Winchester Model 1897.\nShoots incendiary pellets.",
         { Prototype = true, Warden = true, Overlord = true, Cremator = true },
         15, -1, nil, nil, { Warden = 1, Cremator = 1 }, nil, { HORDE.DMG_FIRE }, { HORDE.Infusion_Quality, HORDE.Infusion_Impaling } )
-    HORDE:CreateItem( "Shotgun", "Impaler", "arccw_horde_nailshot", 2250, 5,
+    HORDE:CreateItem( "Shotgun", "Impaler", "arccw_horde_nailshot", 2750, 7    ,
         "Heavily modified Pneumatic Nailgun.\nFires nails in a shotgun style pattern.",
         { Prototype = true, Assault = true, SpecOps = true, Reverend = true, Engineer = true, Berserker = true, Samurai = true, ["Cyborg Ninja"] = true, Warden = true, Overlord = true, Paladin = true },
         15, -1, nil, "arccw/weaponicons/arccw_kf1vlad", nil, nil, { HORDE.DMG_SLASH } )
