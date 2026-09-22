0a4f1acde83f050b7454ad4a2ff962c205e2a755
Skefil <69618043+Joeygi@users.noreply.github.com>
2026-09-21 21:55:28 -0700

    updated weeper summon
    weeper can be summoned instantly like the hulk can, and recharging recalls it

--- gamemodes/horde/gamemode/spells/raise_shadow_weeper.lua
+++ gamemodes/horde/gamemode/spells/raise_shadow_weeper.lua
@@ -1,22 +1,48 @@
-SPELL.PrintName       = "Raise Weeper Spectre"
-SPELL.Weapon          = { "horde_void_projector" }
-SPELL.Mind            = { 75 }
-SPELL.ChargeTime      = { 3 }
-SPELL.ChargeRelease   = nil
-SPELL.Cooldown        = 10
-SPELL.Slot            = HORDE.Spell_Slot_Reload
-SPELL.DamageType      = { HORDE.DMG_PHYSICAL, HORDE.DMG_COLD }
-SPELL.Type            = { HORDE.Spell_Type_Minion }
-SPELL.Icon            = "spells/raise_shadow_hulk.png"
-SPELL.Description     = [[Raises a Weeper Spectre created using dark matter. Weeper Spectre will attack nearby enemies with a frost nova, inflicting Cold damage and inflicting Frostbite. You can create at most 1 Weeper Spectre.]]
-SPELL.Fire            = function( ply )
+SPELL.PrintName             = "Raise Weeper Spectre"
+SPELL.Weapon                = { "horde_void_projector" }
+
+SPELL.Mind                  = { 60, 0 }
+SPELL.ChargeTime            = { 0, 0.5 }
+SPELL.ChargeRelease         = nil
+SPELL.Cooldown              = 10
+
+SPELL.Slot                  = HORDE.Spell_Slot_Reload
+SPELL.DamageType            = { HORDE.DMG_PHYSICAL }
+SPELL.Type                  = { HORDE.Spell_Type_Minion }
+
+SPELL.Icon                  = "spells/raise_shadow_hulk.png"
+SPELL.Description           = [[Raises a Weeper Spectre created using dark matter. Weeper Spectre will attack nearby enemies with a frost nova, inflicting Cold damage and building Frostbite. You can create at most 1 Weeper Spectre.]]
+
+SPELL.Fire = function( ply, _, charge_stage )
+
+    if charge_stage == 2 then
+
+        if not HORDE.player_drop_entities[ply:SteamID()] then return end
+
+        for _, ent in pairs( HORDE.player_drop_entities[ply:SteamID()] ) do
+
+            if ent:IsNPC() and ent:GetClass() == "npc_vj_horde_shadow_weeper" then
+
+                local rand = VectorRand() * 50
+                rand.z = 0
+                ent:SetPos( ply:GetPos() + rand )
+
+            end
+
+        end
+        return
+
+    end
+
     return HORDE:RaiseSpectre( ply, {
         weeper_spectre = true
     } )
+
 end
+
 SPELL.Price                 = 2000
 SPELL.Upgrades              = 3
-SPELL.Upgrade_Description   = "Increases minion health and frost nova / melee damage."
+SPELL.Upgrade_Description   = "Increases minion health and damage."
 SPELL.Upgrade_Prices        = function( upgrade_level )
     return 1000 + 100 * upgrade_level
 end
